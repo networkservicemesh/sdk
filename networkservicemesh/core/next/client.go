aea5fc2ec7e1f42b2837e44f5cebf48f2f926c2c
Ed Warnicke <hagbard@gmail.com>
2020-01-15 18:49:53 -0600

    Added networkservicemesh next. (#9)
    * Consolodated CircleCI into one job named 'check'
    
    Signed-off-by: Ed Warnicke <hagbard@gmail.com>
    
    * Added trace networkservicemesh chain element.
    
    Also added the machinery to make it easy to handle the 'replace' for tracking
    the api module.
    
    Signed-off-by: Ed Warnicke <hagbard@gmail.com>
    
    * Added networkservicemesh next.
    
    Signed-off-by: Ed Warnicke <hagbard@gmail.com>

---
+++ networkservicemesh/core/next/client.go
@@ -0,0 +1,62 @@
+// Copyright (c) 2020 Cisco Systems, Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package next
+
+import (
+	"context"
+
+	"github.com/golang/protobuf/ptypes/empty"
+	"google.golang.org/grpc"
+
+	"github.com/networkservicemesh/networkservicemesh/controlplane/api/connection"
+	"github.com/networkservicemesh/networkservicemesh/controlplane/api/networkservice"
+)
+
+type nextClient struct {
+	clients []networkservice.NetworkServiceClient
+	index   int
+}
+
+type ClientWrapper func(networkservice.NetworkServiceClient) networkservice.NetworkServiceClient
+type ClientChainer func(...networkservice.NetworkServiceClient) networkservice.NetworkServiceClient
+
+func NewWrappedNetworkServiceClient(wrapper ClientWrapper, clients ...networkservice.NetworkServiceClient) networkservice.NetworkServiceClient {
+	rv := &nextClient{
+		clients: clients,
+	}
+	for i := range rv.clients {
+		rv.clients[i] = wrapper(rv.clients[i])
+	}
+	return rv
+}
+
+func NewNetworkServiceClient(clients ...networkservice.NetworkServiceClient) networkservice.NetworkServiceClient {
+	return NewWrappedNetworkServiceClient(nil, clients...)
+}
+
+func (n *nextClient) Request(ctx context.Context, request *networkservice.NetworkServiceRequest, opts ...grpc.CallOption) (*connection.Connection, error) {
+	if n.index+1 < len(n.clients) {
+		return n.clients[n.index].Request(withNextClient(ctx, &nextClient{clients: n.clients, index: n.index + 1}), request, opts...)
+	}
+	return n.clients[n.index].Request(withNextClient(ctx, newTailClient()), request, opts...)
+}
+
+func (n *nextClient) Close(ctx context.Context, conn *connection.Connection, opts ...grpc.CallOption) (*empty.Empty, error) {
+	if n.index+1 < len(n.clients) {
+		return n.clients[n.index].Close(withNextClient(ctx, &nextClient{clients: n.clients, index: n.index + 1}), conn, opts...)
+	}
+	return n.clients[n.index].Close(withNextClient(ctx, newTailClient()), conn, opts...)
+}
---
+++ networkservicemesh/core/next/context.go
@@ -0,0 +1,67 @@
+// Copyright (c) 2020 Cisco Systems, Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package next
+
+import (
+	"context"
+
+	"github.com/networkservicemesh/networkservicemesh/controlplane/api/networkservice"
+)
+
+const (
+	nextServerKey contextKeyType = "NextServer"
+	nextClientKey contextKeyType = "NextClient"
+)
+
+type contextKeyType string
+
+// withNextServer -
+//    Wraps 'parent' in a new Context that has the Server networkservice.NetworkServiceServer to be called in the chain
+//    Should only be set in CompositeEndpoint.Request/Close
+func withNextServer(parent context.Context, next networkservice.NetworkServiceServer) context.Context {
+	if parent == nil {
+		parent = context.TODO()
+	}
+	return context.WithValue(parent, nextServerKey, next)
+}
+
+// Server -
+//   Returns the Server networkservice.NetworkServiceServer to be called in the chain from the context.Context
+func Server(ctx context.Context) networkservice.NetworkServiceServer {
+	if rv, ok := ctx.Value(nextServerKey).(networkservice.NetworkServiceServer); ok {
+		return rv
+	}
+	return nil
+}
+
+// withNextClient -
+//    Wraps 'parent' in a new Context that has the Server networkservice.NetworkServiceServer to be called in the chain
+//    Should only be set in CompositeEndpoint.Request/Close
+func withNextClient(parent context.Context, next networkservice.NetworkServiceClient) context.Context {
+	if parent == nil {
+		parent = context.TODO()
+	}
+	return context.WithValue(parent, nextClientKey, next)
+}
+
+// Client -
+//   Returns the Client networkservice.NetworkServiceClient to be called in the chain from the context.Context
+func Client(ctx context.Context) networkservice.NetworkServiceClient {
+	if rv, ok := ctx.Value(nextClientKey).(networkservice.NetworkServiceClient); ok {
+		return rv
+	}
+	return nil
+}
