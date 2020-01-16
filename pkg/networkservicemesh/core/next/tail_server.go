aaa9ec1823870651933c46be08528b7e57585823
Ed Warnicke <hagbard@gmail.com>
2020-01-15 18:55:36 -0600

    Move all go code under pkg (#12)
    * Consolodated CircleCI into one job named 'check'
    
    Signed-off-by: Ed Warnicke <hagbard@gmail.com>
    
    * Added trace networkservicemesh chain element.
    
    Also added the machinery to make it easy to handle the 'replace' for tracking
    the api module.
    
    Signed-off-by: Ed Warnicke <hagbard@gmail.com>
    
    * Added networkservicemesh next.
    
    Signed-off-by: Ed Warnicke <hagbard@gmail.com>
    
    * Added networkservicemesh 'chain'
    
    Signed-off-by: Ed Warnicke <hagbard@gmail.com>
    
    * Add networkservicemesh adapters
    
    Signed-off-by: Ed Warnicke <hagbard@gmail.com>
    
    * Move all go code under pkg/
    
    Signed-off-by: Ed Warnicke <hagbard@gmail.com>

---
+++ pkg/networkservicemesh/core/next/tail_server.go
@@ -0,0 +1,41 @@
+// Copyright (c) 2020 Cisco Systems, Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package next
+
+import (
+	"context"
+
+	"github.com/golang/protobuf/ptypes/empty"
+
+	"github.com/networkservicemesh/networkservicemesh/controlplane/api/connection"
+	"github.com/networkservicemesh/networkservicemesh/controlplane/api/networkservice"
+)
+
+// tailServer is a simple implementation of networkservice.NetworkServiceServer that is called at the end of a chain
+// to insure that we never call a method on a nil object
+type tailServer struct{}
+
+func newTailServer() *tailServer {
+	return &tailServer{}
+}
+
+func (t *tailServer) Request(ctx context.Context, request *networkservice.NetworkServiceRequest) (*connection.Connection, error) {
+	return request.GetConnection(), nil
+}
+
+func (t *tailServer) Close(context.Context, *connection.Connection) (*empty.Empty, error) {
+	return &empty.Empty{}, nil
+}
