bbeabd74ea1c118bac31d7ba0d48bc1bc272e488
Sergey Semenov <lvfx@ya.ru>
2020-02-12 12:55:32 +0700

    Added test and fixed label error
    Signed-off-by: Sergey Semenov <sergey.semenov@xored.com>

--- pkg/networkservice/common/addNames/client.go
+++ pkg/networkservice/common/addNames/client.go
@@ -2,6 +2,7 @@ package addNames
 
 import (
 	"context"
+	"github.com/pkg/errors"
 
 	"github.com/golang/protobuf/ptypes/empty"
 	"google.golang.org/grpc"
@@ -15,19 +16,23 @@ import (
 
 type addNamesClient struct{}
 
-// NewClient - creates a new networkservice.NetworkServiceClient chain element that adds pod, node and cluster names to request from environment variables
+// NewClient - creates a new networkservice.NetworkServiceClient chain element that adds pod, node and cluster names to request from corresponding environment variables
 func NewClient() networkservice.NetworkServiceClient {
 	return &addNamesClient{}
 }
 
 func (a *addNamesClient) Request(ctx context.Context, request *networkservice.NetworkServiceRequest, opts ...grpc.CallOption) (*networkservice.Connection, error) {
 	varNames := map[string]string{"NODE_NAME": "NodeNameKey", "POD_NAME": "PodNameKey", "CLUSTER_NAME": "ClusterNameKey"}
-	labels := request.GetRequestConnection().Labels
+	if request.GetRequestConnection().Labels == nil {
+		request.GetRequestConnection().Labels = make(map[string]string)
+	}
 	for envName, labelName := range varNames {
-		value := os.Getenv(envName) // TODO decide what to do about "" value here
-		// TODO should i check map existence???
-		// TODO warning when overwrite values
-		labels[labelName] = value
+		value, isPresent := os.LookupEnv(envName)
+		if !isPresent {
+			return nil, errors.Errorf("Environment variable %s is not set", envName)
+		}
+		// TODO do i need warning when overwrite values???
+		request.GetRequestConnection().Labels[labelName] = value
 	}
 	return next.Client(ctx).Request(ctx, request, opts...)
 }
--- pkg/networkservice/common/addNames/client_test.go
+++ pkg/networkservice/common/addNames/client_test.go
@@ -1,14 +1,46 @@
 package addNames
 
 import (
+	"context"
 	"github.com/networkservicemesh/api/pkg/api/networkservice"
 	"github.com/networkservicemesh/api/pkg/api/networkservice/mechanisms/kernel"
+	"github.com/networkservicemesh/sdk/pkg/networkservice/core/next"
+	"os"
 	"reflect"
 	"testing"
 )
 
-func Test_roundRobinSelector_SelectEndpoint(t *testing.T) {
-	addNamesClient := NewClient()
+func Test_addNamesClient_Request(t *testing.T) {
+
+	envs := map[string]string{
+		"NODE_NAME":    "AAA",
+		"POD_NAME":     "BBB",
+		"CLUSTER_NAME": "CCC",
+	}
+
+	for name, value := range envs {
+		err := os.Setenv(name, value)
+		if err != nil {
+			t.Errorf("addNamesClient.Request() unable to set up environment variable: %v", err)
+		}
+	}
+
+	want := &networkservice.Connection{
+		Id: "1",
+		Mechanism: &networkservice.Mechanism{
+			Type: kernel.MECHANISM,
+		},
+		Context: &networkservice.ConnectionContext{
+			IpContext: &networkservice.IPContext{
+				DstIpAddr: "172.16.1.2",
+			},
+		},
+		Labels: map[string]string{
+			"NodeNameKey":    "AAA",
+			"PodNameKey":     "BBB",
+			"ClusterNameKey": "CCC",
+		},
+	}
 
 	request := &networkservice.NetworkServiceRequest{
 		Connection: &networkservice.Connection{
@@ -22,6 +54,14 @@ func Test_roundRobinSelector_SelectEndpoint(t *testing.T) {
 				},
 			},
 		},
+	}
+
+	server := next.NewNetworkServiceClient(NewClient())
+
+	got, _ := server.Request(context.Background(), request)
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("addNamesClient.Request() = %v, want %v", got, want)
 	}
 
 }
