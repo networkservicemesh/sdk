fde23dc9da96a3c2e3e32287f2f36f927e20e1f7
Ed Warnicke <hagbard@gmail.com>
2020-02-18 20:57:51 -0600

    Add null networkservice chain element.
    Signed-off-by: Ed Warnicke <hagbard@gmail.com>

---
+++ pkg/networkservice/test/null/client.go
@@ -0,0 +1,45 @@
+// Copyright (c) 2020 Cisco and/or its affiliates.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package null provides simple networkservice chain elements that do nothing by call next and returns what comes
+// back.  This can be very useful in testing.
+package null
+
+import (
+	"context"
+
+	"github.com/golang/protobuf/ptypes/empty"
+	"github.com/networkservicemesh/api/pkg/api/networkservice"
+	"google.golang.org/grpc"
+
+	"github.com/networkservicemesh/sdk/pkg/networkservice/core/next"
+)
+
+type nullClient struct{}
+
+// NewClient - returns a new null client that does nothing but call next.Client(ctx).{Request/Close} and return the result
+//             This is very useful in testing
+func NewClient() networkservice.NetworkServiceClient {
+	return &nullClient{}
+}
+
+func (n *nullClient) Request(ctx context.Context, request *networkservice.NetworkServiceRequest, opts ...grpc.CallOption) (*networkservice.Connection, error) {
+	return next.Client(ctx).Request(ctx, request, opts...)
+}
+
+func (n *nullClient) Close(ctx context.Context, conn *networkservice.Connection, opts ...grpc.CallOption) (*empty.Empty, error) {
+	return next.Client(ctx).Close(ctx, conn, opts...)
+}
