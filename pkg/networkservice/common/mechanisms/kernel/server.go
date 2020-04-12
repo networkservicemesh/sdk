e7e7f886204dc9215e142dbebb9b39aa356be1df
Frederick F. Kautz IV <fkautz@alumni.cmu.edu>
2020-04-11 23:58:07 -0700

    Find inode of container netns
    Signed-off-by: Frederick F. Kautz IV <fkautz@alumni.cmu.edu>
    
    fix format and update actions/checkout to v2
    
    Signed-off-by: Frederick F. Kautz IV <fkautz@alumni.cmu.edu>
    
    remove unused variable in struct
    
    Signed-off-by: Frederick F. Kautz IV <fkautz@alumni.cmu.edu>

---
+++ pkg/networkservice/common/mechanisms/kernel/server.go
@@ -0,0 +1,31 @@
+package kernel
+
+import (
+	"context"
+	"github.com/golang/protobuf/ptypes/empty"
+	"github.com/networkservicemesh/api/pkg/api/networkservice"
+	"github.com/networkservicemesh/api/pkg/api/networkservice/mechanisms/common"
+	"github.com/networkservicemesh/sdk/pkg/tools/inodes"
+	"strconv"
+)
+
+type mechanismsServer struct{}
+
+func (m mechanismsServer) Request(ctx context.Context, req *networkservice.NetworkServiceRequest) (*networkservice.Connection, error) {
+	conn := req.Connection
+	inode := uint64(0)
+	inode, err := inodes.GetInode("/proc/self/net/ns")
+	if err != nil {
+		return nil, err
+	}
+	conn.Mechanism.Parameters[common.NetNSInodeKey] = strconv.FormatUint(inode, 10)
+	return conn, nil
+}
+
+func (m mechanismsServer) Close(context.Context, *networkservice.Connection) (*empty.Empty, error) {
+	panic("implement me")
+}
+
+func NewServer() networkservice.NetworkServiceServer {
+	return mechanismsServer{}
+}
