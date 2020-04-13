3058ad7e5eff3304d7da718ca1fb155a686639ca
Frederick F. Kautz IV <fkautz@alumni.cmu.edu>
2020-04-13 13:12:12 -0700

    Find inode of container netns (#186)
    * Find inode of container netns
    
    Signed-off-by: Frederick F. Kautz IV <fkautz@alumni.cmu.edu>
    
    fix format and update actions/checkout to v2
    
    Signed-off-by: Frederick F. Kautz IV <fkautz@alumni.cmu.edu>
    
    remove unused variable in struct
    
    Signed-off-by: Frederick F. Kautz IV <fkautz@alumni.cmu.edu>
    
    * minor fixes to formatting and comments
    
    Signed-off-by: Frederick F. Kautz IV <fkautz@alumni.cmu.edu>
    
    * go mod tidy
    
    Signed-off-by: Frederick F. Kautz IV <fkautz@alumni.cmu.edu>
    
    * rename inodeinfo to fs
    
    Signed-off-by: Frederick F. Kautz IV <fkautz@alumni.cmu.edu>
    
    * run goimports
    
    Signed-off-by: Frederick F. Kautz IV <fkautz@alumni.cmu.edu>
    
    * run goimports
    
    Signed-off-by: Frederick F. Kautz IV <fkautz@alumni.cmu.edu>
    
    * add kernel.Close()
    
    Signed-off-by: Frederick F. Kautz IV <fkautz@alumni.cmu.edu>

---
+++ pkg/networkservice/common/mechanisms/kernel/server.go
@@ -0,0 +1,51 @@
+// Copyright (c) 2020 Doc.ai and/or its affiliates.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package kernel provides the necessary mechanisms to request and inject a kernel interface.
+package kernel
+
+import (
+	"context"
+	"strconv"
+
+	"github.com/golang/protobuf/ptypes/empty"
+	"github.com/networkservicemesh/api/pkg/api/networkservice"
+	"github.com/networkservicemesh/api/pkg/api/networkservice/mechanisms/common"
+
+	"github.com/networkservicemesh/sdk/pkg/tools/fs"
+)
+
+type mechanismsServer struct{}
+
+func (m mechanismsServer) Request(ctx context.Context, req *networkservice.NetworkServiceRequest) (*networkservice.Connection, error) {
+	conn := req.Connection
+	inode := uint64(0)
+	inode, err := fs.GetInode("/proc/self/net/ns")
+	if err != nil {
+		return nil, err
+	}
+	conn.Mechanism.Parameters[common.NetNSInodeKey] = strconv.FormatUint(inode, 10)
+	return conn, nil
+}
+
+func (m mechanismsServer) Close(context.Context, *networkservice.Connection) (*empty.Empty, error) {
+	return &empty.Empty{}, nil
+}
+
+// NewServer - creates a NetworkServiceServer that requests a kernel interface and populates the netns inode
+func NewServer() networkservice.NetworkServiceServer {
+	return mechanismsServer{}
+}
