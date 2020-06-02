b0b7fe5ab62633061ec79a2f55896236cab8d459
Ed Warnicke <hagbard@gmail.com>
2020-06-02 14:32:48 -0500

    Adapt to spiffev2
    Signed-off-by: Ed Warnicke <hagbard@gmail.com>

--- go.mod
+++ go.mod
@@ -9,36 +9,26 @@ require (
 	github.com/codahale/hdrhistogram v0.0.0-20161010025455-3a0bb77429bd // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/edwarnicke/exechelper v1.0.1
-	github.com/fsnotify/fsnotify v1.4.9 // indirect
 	github.com/golang/protobuf v1.3.5
 	github.com/google/uuid v1.1.1
 	github.com/grpc-ecosystem/grpc-opentracing v0.0.0-20180507213350-8e809c8a8645
 	github.com/matryer/try v0.0.0-20161228173917-9ac251b645a2
-	github.com/mitchellh/mapstructure v1.3.0 // indirect
-	github.com/nats-io/nats-server/v2 v2.1.6 // indirect
 	github.com/nats-io/nats-streaming-server v0.17.0
 	github.com/nats-io/stan.go v0.6.0
 	github.com/networkservicemesh/api v0.0.0-20200525170518-89690ec70489
 	github.com/open-policy-agent/opa v0.16.1
 	github.com/opentracing/opentracing-go v1.1.0
-	github.com/pelletier/go-toml v1.7.0 // indirect
 	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.4.2
-	github.com/spf13/afero v1.2.2 // indirect
-	github.com/spf13/cast v1.3.1 // indirect
-	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.6.3
 	github.com/spiffe/go-spiffe v0.0.0-20200115174642-4e401e3b85fe
+	github.com/spiffe/go-spiffe/v2 v2.0.0-alpha.4.0.20200528145730-dc11d0c74e85
 	github.com/stretchr/testify v1.5.1
 	github.com/uber/jaeger-client-go v2.21.1+incompatible
 	github.com/uber/jaeger-lib v2.2.0+incompatible // indirect
-	go.uber.org/atomic v1.5.1 // indirect
 	go.uber.org/goleak v1.0.0
-	golang.org/x/lint v0.0.0-20200302205851-738671d3881b // indirect
 	golang.org/x/sys v0.0.0-20200501145240-bc7a7d42d5c3
-	golang.org/x/tools v0.0.0-20200417140056-c07e33ef3290 // indirect
 	gonum.org/v1/gonum v0.6.2
-	google.golang.org/grpc v1.27.0
-	gopkg.in/ini.v1 v1.55.0 // indirect
+	google.golang.org/grpc v1.27.1
 )
---
+++ pkg/tools/spiffejwt/token.go
@@ -0,0 +1,67 @@
+// Copyright (c) 2020 Cisco and/or its affiliates.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package spiffejwt
+
+import (
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/pkg/errors"
+	"github.com/spiffe/go-spiffe/v2/svid/x509svid"
+	"google.golang.org/grpc/credentials"
+
+	"github.com/networkservicemesh/sdk/pkg/tools/token"
+)
+
+// TokenGeneratorFunc - creates a token.TokenGeneratorFunc that creates spiffe JWT tokens from the cert returned by getCert()
+func TokenGeneratorFunc(source x509svid.Source, maxTokenLifeTime time.Duration) token.GeneratorFunc {
+	return func(authInfo credentials.AuthInfo) (string, time.Time, error) {
+		ownSVID, err := source.GetX509SVID()
+		if err != nil {
+			return "", time.Time{}, errors.Wrap(err, "Error creating Token")
+		}
+
+		expireTime := time.Now().Add(maxTokenLifeTime)
+		if ownSVID.Certificates[0].NotAfter.Before(expireTime) {
+			expireTime = ownSVID.Certificates[0].NotAfter
+		}
+		if err != nil {
+			return "", time.Time{}, errors.Wrap(err, "Error creating Token")
+		}
+		claims := jwt.StandardClaims{
+			Subject:   ownSVID.ID.String(),
+			ExpiresAt: expireTime.Unix(),
+		}
+		if authInfo != nil {
+			if tlsInfo, ok := authInfo.(credentials.TLSInfo); ok {
+				if len(tlsInfo.State.PeerCertificates) > 0 {
+					peerCert := tlsInfo.State.PeerCertificates[0]
+					peerSpiffeID, err2 := x509svid.IDFromCert(peerCert)
+					if err2 != nil {
+						return "", time.Time{}, err2
+					}
+					if peerCert.NotAfter.Before(expireTime) {
+						expireTime = peerCert.NotAfter
+					}
+					claims.Audience = peerSpiffeID.String()
+				}
+			}
+		}
+		tok, err := jwt.NewWithClaims(jwt.SigningMethodES256, claims).SignedString(ownSVID.PrivateKey)
+		return tok, expireTime, err
+	}
+}
