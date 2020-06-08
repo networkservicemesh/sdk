be3d6efcfa550203fcd629f077067c3f7d9aa64e
Andrey Sobolev <haiodo@gmail.com>
2020-06-08 16:08:25 +0700

    Fix discovery client
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>

--- pkg/registry/core/next/context.go
+++ pkg/registry/core/next/context.go
@@ -127,10 +127,17 @@ func withNextDiscoveryClient(parent context.Context, next registry.NetworkServic
 // DiscoveryClient -
 //   Returns the DiscoveryClient registry.NetworkServiceDiscoveryClient to be called in the chain from the context.Context
 func DiscoveryClient(ctx context.Context) registry.NetworkServiceDiscoveryClient {
-	if rv, ok := ctx.Value(nextDiscoveryClientKey).(registry.NetworkServiceDiscoveryClient); ok {
+	rv, ok := ctx.Value(nextDiscoveryClientKey).(registry.NetworkServiceDiscoveryClient)
+	if !ok {
+		server, ok := ctx.Value(nextDiscoveryServerKey).(registry.NetworkServiceDiscoveryServer)
+		if ok {
+			rv = adapters.NewDiscoveryServerToClient(server, DiscoveryClient)
+		}
+	}
+	if rv != nil {
 		return rv
 	}
-	return nil
+	return tail.NewTailDiscoveryClient()
 }
 
 // NSMRegistryClient -
