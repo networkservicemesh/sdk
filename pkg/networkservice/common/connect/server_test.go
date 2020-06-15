281f824ba05713cf2ee4a4d5d768c78134b4ebf0
Andrey Sobolev <haiodo@users.noreply.github.com>
2020-06-16 01:33:09 +0700

    Connect chain rework (#304)
    * Complex connect chain update
    
    + support cleanups
    + few proper tests
    
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>
    
    * Review fixes
    
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>
    
    * Simplify and review comments
    
    Simplify and review comments
    
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>

---
+++ pkg/networkservice/common/connect/server_test.go
@@ -0,0 +1,234 @@
+// Copyright (c) 2020 Doc.ai and/or its affiliates.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package connect
+
+import (
+	"context"
+	"fmt"
+	"net/url"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/networkservicemesh/sdk/pkg/networkservice/chains/client"
+	"github.com/networkservicemesh/sdk/pkg/networkservice/core/adapters"
+	"github.com/networkservicemesh/sdk/pkg/networkservice/utils/testnse"
+
+	"github.com/networkservicemesh/api/pkg/api/networkservice"
+	"google.golang.org/grpc"
+
+	"github.com/stretchr/testify/require"
+	"go.uber.org/goleak"
+	"google.golang.org/grpc/credentials"
+
+	"github.com/networkservicemesh/sdk/pkg/networkservice/common/clienturl"
+)
+
+func TokenGenerator(peerAuthInfo credentials.AuthInfo) (token string, expireTime time.Time, err error) {
+	return "TestToken", time.Date(3000, 1, 1, 1, 1, 1, 1, time.UTC), nil
+}
+
+type nseTest struct {
+	ctx      context.Context
+	cancel   context.CancelFunc
+	listenOn *url.URL
+	nse      networkservice.NetworkServiceServer
+	nseSrv   *grpc.Server
+}
+
+func (nseT *nseTest) Stop() {
+	nseT.cancel()
+	nseT.nseSrv.Stop()
+}
+
+func (nseT *nseTest) Setup() {
+	nseT.ctx, nseT.cancel = context.WithTimeout(context.Background(), 5*time.Second)
+	nseT.listenOn = &url.URL{Scheme: "tcp", Path: "127.0.0.1:0"}
+
+	nseT.nse, nseT.nseSrv, _ = testnse.NewNSE(nseT.ctx, nseT.listenOn, func(request *networkservice.NetworkServiceRequest) {
+		request.Connection.Labels = map[string]string{"ok": "all is ok"}
+	})
+}
+
+func (nseT *nseTest) newNSEContext(ctx context.Context) context.Context {
+	return clienturl.WithClientURL(ctx, &url.URL{Scheme: "tcp", Path: nseT.listenOn.Path})
+}
+
+func TestConnectServerShouldNotPanicOnRequest(t *testing.T) {
+	defer goleak.VerifyNone(t)
+
+	nseT := &nseTest{}
+	nseT.Setup()
+	defer nseT.Stop()
+
+	t.Run("Check Request", func(t *testing.T) {
+		require.NotPanics(t, func() {
+			s := NewServer(func(ctx context.Context, cc grpc.ClientConnInterface) networkservice.NetworkServiceClient {
+				return adapters.NewServerToClient(nseT.nse)
+			}, grpc.WithInsecure())
+			clientURLCtx := nseT.newNSEContext(context.Background())
+			conn, err := s.Request(clientURLCtx, &networkservice.NetworkServiceRequest{
+				Connection: &networkservice.Connection{
+					Id: "1",
+				},
+			})
+			require.Nil(t, err)
+			require.NotNil(t, conn)
+			require.Equal(t, "all is ok", conn.Labels["ok"])
+			_, err = s.Close(clientURLCtx, &networkservice.Connection{
+				Id: "1",
+			})
+			require.Nil(t, err)
+		})
+	})
+	t.Run("Close Id", func(t *testing.T) {
+		require.NotPanics(t, func() {
+			s := NewServer(func(ctx context.Context, cc grpc.ClientConnInterface) networkservice.NetworkServiceClient {
+				return adapters.NewServerToClient(nseT.nse)
+			}, grpc.WithInsecure())
+			clientURLCtx := nseT.newNSEContext(context.Background())
+			conn, err := s.Request(clientURLCtx, &networkservice.NetworkServiceRequest{
+				Connection: &networkservice.Connection{
+					Id: "1",
+				},
+			})
+			require.Nil(t, err)
+			require.NotNil(t, conn)
+			require.Equal(t, "all is ok", conn.Labels["ok"])
+
+			// Do not pass clientURL
+			_, err = s.Close(context.Background(), &networkservice.Connection{
+				Id: "1",
+			})
+			require.Nil(t, err)
+		})
+	})
+	t.Run("Check no clientURL", func(t *testing.T) {
+		require.NotPanics(t, func() {
+			s := NewServer(func(ctx context.Context, cc grpc.ClientConnInterface) networkservice.NetworkServiceClient {
+				return adapters.NewServerToClient(nseT.nse)
+			}, grpc.WithInsecure())
+
+			conn, err := s.Request(context.Background(), &networkservice.NetworkServiceRequest{
+				Connection: &networkservice.Connection{
+					Id: "1",
+				},
+			})
+			require.NotNil(t, err)
+			require.Nil(t, conn)
+		})
+	})
+	t.Run("Request without client URL", func(t *testing.T) {
+		require.NotPanics(t, func() {
+			s := NewServer(func(ctx context.Context, cc grpc.ClientConnInterface) networkservice.NetworkServiceClient {
+				return adapters.NewServerToClient(nseT.nse)
+			}, grpc.WithInsecure())
+			clientURLCtx := nseT.newNSEContext(context.Background())
+			conn, err := s.Request(clientURLCtx, &networkservice.NetworkServiceRequest{
+				Connection: &networkservice.Connection{
+					Id: "1",
+				},
+			})
+			require.Nil(t, err)
+			require.NotNil(t, conn)
+			require.Equal(t, "all is ok", conn.Labels["ok"])
+
+			// Request again
+			conn, err = s.Request(context.Background(), &networkservice.NetworkServiceRequest{
+				Connection: &networkservice.Connection{
+					Id: "1",
+				},
+			})
+			require.Nil(t, err)
+			require.NotNil(t, conn)
+			require.Equal(t, "all is ok", conn.Labels["ok"])
+
+			// Do not pass clientURL
+			_, err = s.Close(context.Background(), &networkservice.Connection{
+				Id: "1",
+			})
+			require.Nil(t, err)
+		})
+	})
+}
+
+func TestDialFactoryRequest(t *testing.T) {
+	defer goleak.VerifyNone(t)
+
+	nseT := &nseTest{}
+	nseT.Setup()
+	defer nseT.Stop()
+	require.NotPanics(t, func() {
+		s := NewServer(client.NewClientFactory("nsc", nil, TokenGenerator),
+			WithDialOptionFactory(func(ctx context.Context, clientURL *url.URL) []grpc.DialOption {
+				return []grpc.DialOption{grpc.WithInsecure()}
+			}))
+		ctx := nseT.newNSEContext(context.Background())
+		conn, err := s.Request(ctx, &networkservice.NetworkServiceRequest{
+			Connection: &networkservice.Connection{
+				Id: "1",
+				Path: &networkservice.Path{
+					PathSegments: []*networkservice.PathSegment{},
+				},
+			},
+		})
+		require.Nil(t, err)
+		require.NotNil(t, conn)
+
+		_, err = s.Close(ctx, conn)
+
+		require.Nil(t, err)
+	})
+}
+func TestParallelDial(t *testing.T) {
+	defer goleak.VerifyNone(t)
+
+	nseT := &nseTest{}
+	nseT.Setup()
+	defer nseT.Stop()
+
+	s := NewServer(client.NewClientFactory("nsc", nil, TokenGenerator),
+		WithDialOptionFactory(func(ctx context.Context, clientURL *url.URL) []grpc.DialOption {
+			return []grpc.DialOption{grpc.WithInsecure()}
+		}))
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		j := i
+		ctx := nseT.newNSEContext(context.Background())
+		go func() {
+			defer wg.Done()
+			for k := 0; k < 10; k++ {
+				conn, err := s.Request(ctx, &networkservice.NetworkServiceRequest{
+					Connection: &networkservice.Connection{
+						Id: fmt.Sprintf("%d", j),
+						Path: &networkservice.Path{
+							PathSegments: []*networkservice.PathSegment{},
+						},
+					},
+				})
+				require.Nil(t, err)
+				require.NotNil(t, conn)
+
+				_, err = s.Close(ctx, conn)
+
+				require.Nil(t, err)
+			}
+		}()
+	}
+	wg.Wait()
+}
---
+++ pkg/networkservice/utils/testnse/testnse.go
@@ -0,0 +1,71 @@
+// Copyright (c) 2020 Doc.ai and/or its affiliates.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package testnse define a test NSE listening on passed URL.
+package testnse
+
+import (
+	"context"
+	"net/url"
+
+	"github.com/networkservicemesh/sdk/pkg/tools/grpcutils"
+
+	"github.com/golang/protobuf/ptypes/empty"
+	"github.com/networkservicemesh/api/pkg/api/networkservice"
+	"google.golang.org/grpc"
+)
+
+type nseImpl struct {
+	server    *grpc.Server
+	ctx       context.Context
+	cancel    context.CancelFunc
+	listenOn  *url.URL
+	errorChan <-chan error
+	update    func(request *networkservice.NetworkServiceRequest)
+}
+
+func (d *nseImpl) MonitorConnections(selector *networkservice.MonitorScopeSelector, server networkservice.MonitorConnection_MonitorConnectionsServer) error {
+	// Just wait to end
+	<-server.Context().Done()
+	return nil
+}
+
+// NewNSE construct a new NSE with callback up and running on grpc server, listenOn is updated if :0 port is passed.
+func NewNSE(ctx context.Context, listenOn *url.URL, update func(request *networkservice.NetworkServiceRequest)) (networkservice.NetworkServiceServer, *grpc.Server, <-chan error) {
+	nse := &nseImpl{
+		listenOn: listenOn,
+		server:   grpc.NewServer(),
+		update:   update,
+	}
+	networkservice.RegisterNetworkServiceServer(nse.server, nse)
+	networkservice.RegisterMonitorConnectionServer(nse.server, nse)
+
+	nse.ctx, nse.cancel = context.WithCancel(ctx)
+	nse.errorChan = grpcutils.ListenAndServe(nse.ctx, nse.listenOn, nse.server)
+	return nse, nse.server, nse.errorChan
+}
+
+func (d *nseImpl) Request(_ context.Context, request *networkservice.NetworkServiceRequest) (*networkservice.Connection, error) {
+	request.Connection.Labels = map[string]string{}
+	if d.update != nil {
+		d.update(request)
+	}
+	return request.GetConnection(), nil
+}
+
+func (d *nseImpl) Close(_ context.Context, _ *networkservice.Connection) (*empty.Empty, error) {
+	return &empty.Empty{}, nil
+}
