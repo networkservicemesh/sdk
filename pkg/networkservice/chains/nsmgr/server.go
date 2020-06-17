77c1a2e30408f3fefa09bf06941f1e7410688403
Andrey Sobolev <haiodo@users.noreply.github.com>
2020-06-18 03:41:37 +0700

    Nsmgr chain update (#295)
    * Nsmgr main chain element.
    
    Nsmgr main chain element.
    
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>
    
    * Move callbacks to nsmgr application.
    
    Move callbacks to nsmgr application.
    
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>
    
    * Update to new API
    
    Update to new API
    
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>

--- pkg/networkservice/chains/nsmgr/server.go
+++ pkg/networkservice/chains/nsmgr/server.go
@@ -14,13 +14,19 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package nsmgr provides a Network Service Manager (nsmgr), but interface and implementation
+// Package nsmgr provides a Network Service Manager (nsmgrServer), but interface and implementation
 package nsmgr
 
 import (
 	"github.com/networkservicemesh/api/pkg/api/networkservice"
 	"github.com/networkservicemesh/api/pkg/api/registry"
 	"google.golang.org/grpc"
+
+	"github.com/networkservicemesh/sdk/pkg/registry/common/setid"
+	"github.com/networkservicemesh/sdk/pkg/registry/common/seturl"
+	chain_registry "github.com/networkservicemesh/sdk/pkg/registry/core/chain"
+	"github.com/networkservicemesh/sdk/pkg/registry/core/nextwrap"
+	"github.com/networkservicemesh/sdk/pkg/registry/memory"
 
 	"github.com/networkservicemesh/sdk/pkg/networkservice/chains/client"
 	"github.com/networkservicemesh/sdk/pkg/networkservice/chains/endpoint"
@@ -30,7 +36,6 @@ import (
 	"github.com/networkservicemesh/sdk/pkg/networkservice/common/roundrobin"
 	"github.com/networkservicemesh/sdk/pkg/networkservice/core/adapters"
 	adapter_registry "github.com/networkservicemesh/sdk/pkg/registry/core/adapters"
-	chain_registry "github.com/networkservicemesh/sdk/pkg/registry/core/chain"
 	"github.com/networkservicemesh/sdk/pkg/tools/addressof"
 	"github.com/networkservicemesh/sdk/pkg/tools/token"
 )
@@ -38,51 +43,98 @@ import (
 // Nsmgr - A simple combintation of the Endpoint, registry.NetworkServiceRegistryServer, and registry.NetworkServiceDiscoveryServer interfaces
 type Nsmgr interface {
 	endpoint.Endpoint
+
 	NetworkServiceRegistryServer() registry.NetworkServiceRegistryServer
 	NetworkServiceEndpointRegistryServer() registry.NetworkServiceEndpointRegistryServer
 }
 
-type nsmgr struct {
+type nsmgrServer struct {
 	endpoint.Endpoint
 	nsServer  registry.NetworkServiceRegistryServer
 	nseServer registry.NetworkServiceEndpointRegistryServer
 }
 
-func (n *nsmgr) NetworkServiceRegistryServer() registry.NetworkServiceRegistryServer {
+func (n *nsmgrServer) NetworkServiceRegistryServer() registry.NetworkServiceRegistryServer {
 	return n.nsServer
 }
 
-func (n *nsmgr) NetworkServiceEndpointRegistryServer() registry.NetworkServiceEndpointRegistryServer {
+func (n *nsmgrServer) NetworkServiceEndpointRegistryServer() registry.NetworkServiceEndpointRegistryServer {
 	return n.nseServer
 }
 
 // NewServer - Creates a new Nsmgr
-//           name - name of the Nsmgr
+//           nsmRegistration - Nsmgr registration
 //           authzServer - authorization server chain element
-//           registryCC - client connection to reach the upstream registry
-func NewServer(name string, authzServer networkservice.NetworkServiceServer, tokenGenerator token.GeneratorFunc, registryCC grpc.ClientConnInterface, dialOptions ...grpc.DialOption) Nsmgr {
-	rv := &nsmgr{}
+//           tokenGenerator - authorization token generator
+//           registryCC - client connection to reach the upstream registry, could be nil, in this case only in memory storage will be used.
+// 			 clientDialOptions -  a grpc.DialOption's to be passed to GRPC connections.
+func NewServer(nsmRegistration *registry.NetworkServiceEndpoint, authzServer networkservice.NetworkServiceServer, tokenGenerator token.GeneratorFunc, registryCC grpc.ClientConnInterface, clientDialOptions ...grpc.DialOption) Nsmgr {
+	rv := &nsmgrServer{}
 
+	var localbypassRegistryServer registry.NetworkServiceEndpointRegistryServer
+
+	nsRegistry := newRemoteNSServer(registryCC)
+	if nsRegistry == nil {
+		// Use memory registry if no registry is passed
+		nsRegistry = memory.NewNetworkServiceRegistryServer()
+	}
+
+	nseRegistry := newRemoteNSEServer(registryCC)
+	if nseRegistry == nil {
+		nseRegistry = chain_registry.NewNetworkServiceEndpointRegistryServer(
+			setid.NewNetworkServiceEndpointRegistryServer(),  // If no remote registry then assign ID.
+			memory.NewNetworkServiceEndpointRegistryServer(), // Memory registry to store result inside.
+		)
+	}
+
+	// Construct Endpoint
 	rv.Endpoint = endpoint.NewServer(
-		name,
+		nsmRegistration.Name,
 		authzServer,
 		tokenGenerator,
-		discover.NewServer(registry.NewNetworkServiceRegistryClient(registryCC), registry.NewNetworkServiceEndpointRegistryClient(registryCC)),
+		discover.NewServer(adapter_registry.NetworkServiceServerToClient(nsRegistry), adapter_registry.NetworkServiceEndpointServerToClient(nseRegistry)),
 		roundrobin.NewServer(),
-		localbypass.NewServer(&rv.nseServer),
-		connect.NewServer(client.NewClientFactory(name, addressof.NetworkServiceClient(adapters.NewServerToClient(rv)), tokenGenerator), dialOptions...),
+		localbypass.NewServer(&localbypassRegistryServer),
+		connect.NewServer(
+			client.NewClientFactory(nsmRegistration.Name,
+				addressof.NetworkServiceClient(
+					adapters.NewServerToClient(rv)),
+				tokenGenerator),
+			clientDialOptions...),
 	)
-	rv.nsServer = chain_registry.NewNetworkServiceRegistryServer(
-		adapter_registry.NetworkServiceClientToServer(registry.NewNetworkServiceRegistryClient(registryCC)),
-	)
+
+	rv.nsServer = chain_registry.NewNetworkServiceRegistryServer(nsRegistry)
+
 	rv.nseServer = chain_registry.NewNetworkServiceEndpointRegistryServer(
-		adapter_registry.NetworkServiceEndpointClientToServer(registry.NewNetworkServiceEndpointRegistryClient(registryCC)),
+		seturl.NewServer(nsmRegistration.Url), // Remember endpoint URL
+		nseRegistry,                           // Register NSE inside Remote registry with ID assigned
+		localbypassRegistryServer,             // Store endpoint Id to EndpointURL for local access.
 	)
+
 	return rv
 }
 
-func (n *nsmgr) Register(s *grpc.Server) {
+func newRemoteNSServer(cc grpc.ClientConnInterface) registry.NetworkServiceRegistryServer {
+	if cc != nil {
+		return adapter_registry.NetworkServiceClientToServer(
+			nextwrap.NewNetworkServiceRegistryClient(
+				registry.NewNetworkServiceRegistryClient(cc)))
+	}
+	return nil
+}
+func newRemoteNSEServer(cc grpc.ClientConnInterface) registry.NetworkServiceEndpointRegistryServer {
+	if cc != nil {
+		return adapter_registry.NetworkServiceEndpointClientToServer(
+			nextwrap.NewNetworkServiceEndpointRegistryClient(
+				registry.NewNetworkServiceEndpointRegistryClient(cc)))
+	}
+	return nil
+}
+
+func (n *nsmgrServer) Register(s *grpc.Server) {
 	n.Endpoint.Register(s)
+
+	// Register registry
 	registry.RegisterNetworkServiceRegistryServer(s, n.NetworkServiceRegistryServer())
 	registry.RegisterNetworkServiceEndpointRegistryServer(s, n.NetworkServiceEndpointRegistryServer())
 }
---
+++ pkg/networkservice/chains/nsmgr/server_test.go
@@ -0,0 +1,133 @@
+// Copyright (c) 2020 Doc.ai and/or its affiliates.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package nsmgr_test define a tests for NSMGR chain element.
+package nsmgr_test
+
+import (
+	"context"
+	"net/url"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/networkservicemesh/api/pkg/api/networkservice"
+	"github.com/networkservicemesh/api/pkg/api/networkservice/mechanisms/cls"
+	"github.com/networkservicemesh/api/pkg/api/networkservice/mechanisms/kernel"
+	"github.com/networkservicemesh/api/pkg/api/registry"
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/grpclog"
+
+	"github.com/networkservicemesh/sdk/pkg/networkservice/chains/client"
+	"github.com/networkservicemesh/sdk/pkg/networkservice/chains/nsmgr"
+	"github.com/networkservicemesh/sdk/pkg/networkservice/common/authorize"
+	"github.com/networkservicemesh/sdk/pkg/networkservice/utils/testnse"
+	"github.com/networkservicemesh/sdk/pkg/tools/grpcutils"
+)
+
+func TokenGenerator(peerAuthInfo credentials.AuthInfo) (token string, expireTime time.Time, err error) {
+	return "TestToken", time.Date(3000, 1, 1, 1, 1, 1, 1, time.UTC), nil
+}
+
+func serverNSM(ctx context.Context, listenOn *url.URL, server nsmgr.Nsmgr) (*grpc.Server, context.CancelFunc, <-chan error) {
+	s := grpc.NewServer()
+	server.Register(s)
+
+	cancelCtx, cancel := context.WithCancel(ctx)
+	errorChan := grpcutils.ListenAndServe(cancelCtx, listenOn, s)
+	return s, cancel, errorChan
+}
+
+func newClient(ctx context.Context, u *url.URL) (*grpc.ClientConn, error) {
+	clientCtx, clientCancelFunc := context.WithTimeout(ctx, 10*time.Second)
+	defer clientCancelFunc()
+	return grpc.DialContext(clientCtx, grpcutils.URLToTarget(u),
+		grpc.WithInsecure(),
+		grpc.WithBlock())
+}
+
+func TestNSmgrEndpointCallback(t *testing.T) {
+	grpclog.SetLoggerV2(grpclog.NewLoggerV2(os.Stdout, os.Stdout, os.Stderr))
+
+	ctx, cancel := context.WithTimeout(context.Background(), 150*time.Second)
+	defer cancel()
+
+	// Serve endpoint
+	nseURL := &url.URL{Scheme: "tcp", Host: "127.0.0.1:0"}
+	nseServer, nseGrpcServer, nseErrChan := testnse.NewNSE(ctx, nseURL, func(request *networkservice.NetworkServiceRequest) {
+		// Update labels to be sure all code is executed.
+		request.GetConnection().Labels = map[string]string{"perform": "ok"}
+	})
+	logrus.Infof("NSE listenON: %v", nseURL.String())
+	require.NotNil(t, nseServer)
+	require.NotNil(t, nseErrChan)
+	require.NotNil(t, nseGrpcServer)
+
+	nsmgrReg := &registry.NetworkServiceEndpoint{
+		Name: "nsmgr",
+		Url:  "tcp://127.0.0.1:5001",
+	}
+
+	// Server NSMGR, Use in memory registry server
+	mgr := nsmgr.NewServer(nsmgrReg, authorize.NewServer(), TokenGenerator, nil, grpc.WithInsecure(), grpc.WithDefaultCallOptions(grpc.WaitForReady(true)))
+	nsmURL := &url.URL{Scheme: "tcp", Host: "127.0.0.1:0"}
+	mgrGrpcSrv, mgrGrpcCancel, mgrErr := serverNSM(ctx, nsmURL, mgr)
+	require.NotNil(t, mgrGrpcSrv)
+	require.NotNil(t, mgrErr)
+	defer mgrGrpcCancel()
+	nsmgrReg.Url = nsmURL.String()
+	logrus.Infof("NSMGR listenON: %v", nsmURL.String())
+
+	// Register network service.
+	nsService, err := mgr.NetworkServiceRegistryServer().Register(context.Background(), &registry.NetworkService{
+		Name: "my-service",
+	})
+	require.Nil(t, err)
+
+	// Register Endpoint
+	var nseReg *registry.NetworkServiceEndpoint
+	nseReg, err = mgr.NetworkServiceEndpointRegistryServer().Register(context.Background(), &registry.NetworkServiceEndpoint{
+		NetworkServiceName: []string{nsService.Name},
+		Url:                nseURL.String(),
+	})
+	require.Nil(t, err)
+	require.NotEqual(t, "", nseReg.Name)
+	require.NotNil(t, nseReg)
+
+	var nsmClient grpc.ClientConnInterface
+	nsmClient, err = newClient(ctx, nsmURL)
+	require.Nil(t, err)
+	cl := client.NewClient(context.Background(), "nsc-1", nil, TokenGenerator, nsmClient)
+
+	var connection *networkservice.Connection
+
+	connection, err = cl.Request(ctx, &networkservice.NetworkServiceRequest{
+		MechanismPreferences: []*networkservice.Mechanism{
+			{Cls: cls.LOCAL, Type: kernel.MECHANISM},
+		},
+		Connection: &networkservice.Connection{
+			Id:             "1",
+			NetworkService: "my-service",
+			Context:        &networkservice.ConnectionContext{},
+		},
+	})
+	require.Nil(t, err)
+	require.NotNil(t, connection)
+	require.Equal(t, 2, len(connection.Path.PathSegments))
+}
