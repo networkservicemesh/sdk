0d403addab55711c9e69e9057f0ce372f284464d
Denis Tingaikin <49399980+denis-tingajkin@users.noreply.github.com>
2020-06-29 14:42:06 +0700

    Add refresh registry chain element (#347)
    * Add refresh chain element
    
    Signed-off-by: denis-tingajkin <denis.tingajkin@xored.com>
    
    * apply review comments
    
    Signed-off-by: denis-tingajkin <denis.tingajkin@xored.com>

---
+++ pkg/registry/common/refresh/nse_registry_client_test.go
@@ -0,0 +1,74 @@
+// Copyright (c) 2020 Doc.ai and/or its affiliates.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package refresh_test
+
+import (
+	"context"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/golang/protobuf/ptypes/empty"
+	"github.com/golang/protobuf/ptypes/timestamp"
+	"github.com/networkservicemesh/api/pkg/api/registry"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/goleak"
+	"google.golang.org/grpc"
+
+	"github.com/networkservicemesh/sdk/pkg/registry/common/refresh"
+)
+
+type testNSEClient struct {
+	sync.Mutex
+	requestCount int
+}
+
+func (t *testNSEClient) Register(ctx context.Context, in *registry.NetworkServiceEndpoint, opts ...grpc.CallOption) (*registry.NetworkServiceEndpoint, error) {
+	t.Lock()
+	defer t.Unlock()
+	t.requestCount++
+	return in, nil
+}
+
+func (t *testNSEClient) Find(ctx context.Context, in *registry.NetworkServiceEndpointQuery, opts ...grpc.CallOption) (registry.NetworkServiceEndpointRegistry_FindClient, error) {
+	panic("implement me")
+}
+
+func (t *testNSEClient) Unregister(ctx context.Context, in *registry.NetworkServiceEndpoint, opts ...grpc.CallOption) (*empty.Empty, error) {
+	return nil, nil
+}
+
+func TestNewNetworkServiceEndpointRegistryClient(t *testing.T) {
+	defer goleak.VerifyNone(t)
+	testClient := testNSEClient{}
+	refreshClient := refresh.NewNetworkServiceEndpointRegistryClient(&testClient, refresh.WithRetryPeriod(time.Millisecond*100))
+	expirationTime := time.Now().Add(time.Millisecond * 100)
+	_, err := refreshClient.Register(context.Background(), &registry.NetworkServiceEndpoint{
+		Name: "nse-1",
+		ExpirationTime: &timestamp.Timestamp{
+			Seconds: expirationTime.Unix(),
+			Nanos:   int32(expirationTime.Nanosecond()),
+		},
+	})
+	require.Nil(t, err)
+	<-time.After(time.Millisecond * 100)
+	_, err = refreshClient.Unregister(context.Background(), &registry.NetworkServiceEndpoint{Name: "nse-1"})
+	require.Nil(t, err)
+	testClient.Lock()
+	defer testClient.Unlock()
+	require.Equal(t, 1, testClient.requestCount)
+}
