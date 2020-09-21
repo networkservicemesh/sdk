c057c01651a17f58209017cf7976bcce3759108b
Ed Warnicke <hagbard@gmail.com>
2020-09-20 21:58:30 -0500

    Add sendfd and recvfd to send and recv any relavent fds for mechanisms. (#467)
    Signed-off-by: Ed Warnicke <hagbard@gmail.com>

--- go.mod
+++ go.mod
@@ -8,14 +8,13 @@ require (
 	github.com/codahale/hdrhistogram v0.0.0-20161010025455-3a0bb77429bd // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/edwarnicke/exechelper v1.0.1
-	github.com/edwarnicke/grpcfd v0.0.0-20200803222316-dd18d0d4c179
+	github.com/edwarnicke/grpcfd v0.0.0-20200920223154-d5b6e1f19bd0
+	github.com/edwarnicke/serialize v1.0.0
 	github.com/fsnotify/fsnotify v1.4.7
 	github.com/ghodss/yaml v1.0.0
 	github.com/golang/protobuf v1.4.2
 	github.com/google/uuid v1.1.1
 	github.com/grpc-ecosystem/grpc-opentracing v0.0.0-20180507213350-8e809c8a8645
-	github.com/hashicorp/go-uuid v1.0.1 // indirect
-	github.com/hashicorp/golang-lru v0.5.1 // indirect
 	github.com/matryer/try v0.0.0-20161228173917-9ac251b645a2
 	github.com/nats-io/nats-streaming-server v0.17.0
 	github.com/nats-io/stan.go v0.6.0
@@ -28,16 +27,10 @@ require (
 	github.com/stretchr/testify v1.5.1
 	github.com/uber/jaeger-client-go v2.21.1+incompatible
 	github.com/uber/jaeger-lib v2.2.0+incompatible // indirect
-	go.uber.org/atomic v1.6.0 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/goleak v1.0.1-0.20200717213025-100c34bdc9d6
-	golang.org/x/exp v0.0.0-20191030013958-a1ab85dbe136 // indirect
-	golang.org/x/lint v0.0.0-20200302205851-738671d3881b // indirect
-	golang.org/x/sys v0.0.0-20200610111108-226ff32320da
-	golang.org/x/text v0.3.2 // indirect
-	golang.org/x/tools v0.0.0-20200717024301-6ddee64345a6 // indirect
+	golang.org/x/sys v0.0.0-20200916084744-dbad9cb7cb7a
 	gonum.org/v1/gonum v0.6.2
-	google.golang.org/genproto v0.0.0-20200615140333-fd031eab31e7 // indirect
-	google.golang.org/grpc v1.30.0
-	google.golang.org/protobuf v1.24.0
-	gopkg.in/yaml.v2 v2.2.4 // indirect
+	google.golang.org/grpc v1.32.0
+	google.golang.org/protobuf v1.25.0
 )
---
+++ pkg/networkservice/common/mechanisms/recvfd/client.go
@@ -0,0 +1,101 @@
+// Copyright (c) 2020 Cisco and/or its affiliates.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build !windows
+
+package recvfd
+
+import (
+	"context"
+	"net/url"
+	"os"
+
+	"github.com/golang/protobuf/ptypes/empty"
+	"github.com/networkservicemesh/api/pkg/api/networkservice"
+	"google.golang.org/grpc"
+
+	"github.com/edwarnicke/grpcfd"
+
+	"github.com/networkservicemesh/sdk/pkg/networkservice/core/next"
+)
+
+type recvFDClient struct {
+	fileMaps perConnectionFileMapMap
+}
+
+// NewClient - returns client chain element to recv FDs over the connection (if possible) for any Mechanism.Parameters[common.InodeURL]
+// url of scheme 'inode'.
+func NewClient() networkservice.NetworkServiceClient {
+	return &recvFDClient{}
+}
+
+func (r *recvFDClient) Request(ctx context.Context, request *networkservice.NetworkServiceRequest, opts ...grpc.CallOption) (*networkservice.Connection, error) {
+	// Get the grpcfd.FDRecver
+	rpcCredentials := grpcfd.PerRPCCredentials(grpcfd.PerRPCCredentialsFromCallOptions(opts...))
+	opts = append(opts, grpc.PerRPCCredentials(rpcCredentials))
+	recv, _ := grpcfd.FromPerRPCCredentials(rpcCredentials)
+
+	// Call the next Client in the chain
+	conn, err := next.Client(ctx).Request(ctx, request, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	// Get the fileMap
+	fileMap, _ := r.fileMaps.LoadOrStore(conn.GetId(), &perConnectionFileMap{
+		filesByInodeURL:    make(map[string]*os.File),
+		inodeURLbyFilename: make(map[string]*url.URL),
+	})
+
+	// Recv the FD and swap theInode to File in the Parameters for the returned connection mechanism
+	err = recvFDAndSwapInodeToFile(ctx, fileMap, conn.GetMechanism().GetParameters(), recv)
+	if err != nil {
+		return nil, err
+	}
+
+	// Return connection
+	return conn, nil
+}
+
+func (r *recvFDClient) Close(ctx context.Context, conn *networkservice.Connection, opts ...grpc.CallOption) (*empty.Empty, error) {
+	// Get the grpcfd.FDRecver
+	rpcCredentials := grpcfd.PerRPCCredentials(grpcfd.PerRPCCredentialsFromCallOptions(opts...))
+	opts = append(opts, grpc.PerRPCCredentials(rpcCredentials))
+	recv, _ := grpcfd.FromPerRPCCredentials(rpcCredentials)
+
+	// Get the fileMap
+	fileMap, _ := r.fileMaps.LoadOrStore(conn.GetId(), &perConnectionFileMap{
+		filesByInodeURL:    make(map[string]*os.File),
+		inodeURLbyFilename: make(map[string]*url.URL),
+	})
+
+	// Whatever happens, clean up the fileMap
+	defer r.fileMaps.Delete(conn.GetId())
+
+	// Call the next Client in the chain
+	_, err := next.Client(ctx).Close(ctx, conn, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	// Recv the FD and swap theInode to File in the Parameters for the returned connection mechanism
+	err = recvFDAndSwapInodeToFile(ctx, fileMap, conn.GetMechanism().GetParameters(), recv)
+	if err != nil {
+		return nil, err
+	}
+
+	return &empty.Empty{}, nil
+}
