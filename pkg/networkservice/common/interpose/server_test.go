3ecc7b753a96f699df503559e96e8041161040c1
Artem Glazychev <artem.glazychev@xored.com>
2020-10-13 18:29:06 +0700

    Fix interpose connectionId if there are many clients in Path
    Signed-off-by: Artem Glazychev <artem.glazychev@xored.com>

--- pkg/networkservice/common/interpose/server_test.go
+++ pkg/networkservice/common/interpose/server_test.go
@@ -88,10 +88,6 @@ func TestCrossNSERequest(t *testing.T) {
 		TokenGenerator,
 		interpose.NewServer("nsmgr", &regServer))
 
-	interposeNSE := endpoint.NewServer(ctx, "interpose-nse",
-		authorize.NewServer(),
-		TokenGenerator)
-
 	regClient := next_reg.NewNetworkServiceEndpointRegistryClient(interpose_reg.NewNetworkServiceEndpointRegistryClient(), adapters2.NetworkServiceEndpointServerToClient(regServer))
 
 	reg, err := regClient.Register(context.Background(), &registry.NetworkServiceEndpoint{
@@ -100,6 +96,11 @@ func TestCrossNSERequest(t *testing.T) {
 	})
 	require.Nil(t, err)
 	require.NotNil(t, reg)
+
+	interposeRegName := reg.Name
+	interposeNSE := endpoint.NewServer(ctx, interposeRegName,
+		authorize.NewServer(),
+		TokenGenerator)
 
 	copyClient := &copyClient{}
 
@@ -138,6 +139,6 @@ func TestCrossNSERequest(t *testing.T) {
 	require.Equal(t, 4, len(copyClient.requests))
 	require.Equal(t, 4, len(segments))
 	require.Equal(t, "nsmgr", segments[1].Name)
-	require.Equal(t, "interpose-nse", segments[2].Name)
+	require.Equal(t, interposeRegName, segments[2].Name)
 	require.Equal(t, "nsmgr", segments[3].Name)
 }
