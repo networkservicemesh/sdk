f63a97ea9e6bee6273c5429dd3d41ca7a4dc8ce8
Vladimir Popov <vladimir.popov@xored.com>
2020-10-19 18:22:47 +0700

    Add serializer
    Signed-off-by: Vladimir Popov <vladimir.popov@xored.com>

---
+++ pkg/tools/serializer/serializer_test.go
@@ -0,0 +1,67 @@
+// Copyright (c) 2020 Doc.ai and/or its affiliates.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package serializer_test
+
+import (
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/networkservicemesh/sdk/pkg/tools/serializer"
+)
+
+const (
+	parallelCount = 100
+)
+
+func TestSerializer_Lock(t *testing.T) {
+	s := serializer.Serializer{}
+	globalUnlock := s.Lock("a")
+
+	wg := sync.WaitGroup{}
+	wg.Add(parallelCount * 2)
+
+	var count int
+	for i := 0; i < parallelCount; i++ {
+		id := i
+		go func() {
+			unlock := s.Lock("a")
+			defer unlock()
+			assert.Equal(t, id, count)
+			count++
+			wg.Done()
+		}()
+		// workers should be locked in the same order they appear
+		<-time.After(time.Millisecond)
+	}
+
+	globalUnlock()
+
+	// we need to be sure that new workers would not break existing FIFO order
+	for i := 0; i < parallelCount; i++ {
+		go func() {
+			unlock := s.Lock("a")
+			defer unlock()
+			count++
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+}
