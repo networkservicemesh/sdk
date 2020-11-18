556282f04005e495e70ad25a3d4c1befbc63ad0e
Ed Warnicke <hagbard@gmail.com>
2020-11-17 19:58:36 -0600

    Add metadata chain element (#590)
    Signed-off-by: Ed Warnicke <hagbard@gmail.com>

---
+++ pkg/networkservice/utils/metadata/client.go
@@ -0,0 +1,52 @@
+// Copyright (c) 2020 Cisco and/or its affiliates.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package metadata
+
+import (
+	"context"
+
+	"github.com/golang/protobuf/ptypes/empty"
+	"github.com/networkservicemesh/api/pkg/api/networkservice"
+	"google.golang.org/grpc"
+
+	"github.com/networkservicemesh/sdk/pkg/networkservice/core/next"
+)
+
+type metaDataClient struct {
+	Map metaDataMap
+}
+
+// NewClient - Enable per Connection.Id metadata for the client
+//             Must come after updatepath.NewClient() in the chain
+func NewClient() networkservice.NetworkServiceClient {
+	return &metaDataClient{}
+}
+
+func (m *metaDataClient) Request(ctx context.Context, request *networkservice.NetworkServiceRequest, opts ...grpc.CallOption) (*networkservice.Connection, error) {
+	ctx = store(ctx, request.GetConnection().GetId(), &m.Map)
+	conn, err := next.Client(ctx).Request(ctx, request, opts...)
+	if err != nil {
+		del(ctx, conn.GetId(), &m.Map)
+		return nil, err
+	}
+	return conn, nil
+}
+
+func (m *metaDataClient) Close(ctx context.Context, conn *networkservice.Connection, opts ...grpc.CallOption) (*empty.Empty, error) {
+	ctx = del(ctx, conn.GetId(), &m.Map)
+	return next.Client(ctx).Close(ctx, conn, opts...)
+}
---
+++ pkg/networkservice/utils/metadata/context.go
@@ -0,0 +1,62 @@
+// Copyright (c) 2020 Cisco and/or its affiliates.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package metadata
+
+import (
+	"context"
+	"sync"
+
+	"github.com/networkservicemesh/api/pkg/api/networkservice"
+)
+
+type metaDataKey struct{}
+
+func store(parent context.Context, id string, mdMap *metaDataMap) context.Context {
+	_, ok := parent.Value(metaDataKey{}).(*metaData)
+	if !ok {
+		md, _ := mdMap.LoadOrStore(id, &metaData{})
+		return context.WithValue(parent, metaDataKey{}, md)
+	}
+	return parent
+}
+
+func del(parent context.Context, id string, mdMap *metaDataMap) context.Context {
+	_, ok := parent.Value(metaDataKey{}).(*metaData)
+	if !ok {
+		md, _ := mdMap.LoadAndDelete(id)
+		return context.WithValue(parent, metaDataKey{}, md)
+	}
+	return parent
+}
+
+// Map - Return the client (or server) per Connection.Id metadata *sync.Map
+func Map(parent context.Context, isClient bool) *sync.Map {
+	m, ok := parent.Value(metaDataKey{}).(*metaData)
+	if !ok || m == nil {
+		panic("please add metadata chain element to your chain")
+	}
+	if isClient {
+		return &m.client
+	}
+	return &m.server
+}
+
+// IsClient - returns true if in implements networkservice.NetworkServiceClient
+func IsClient(in interface{}) bool {
+	_, ok := in.(networkservice.NetworkServiceClient)
+	return ok
+}
