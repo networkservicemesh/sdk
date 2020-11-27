a349bc2e124d0b1074819a44a9c79ba30a734363
Vladimir Popov <vladimir.popov@xored.com>
2020-11-27 15:03:47 +0700

    Rework clientmap.RefcountMap, add tests, add bench tests.
    Signed-off-by: Vladimir Popov <vladimir.popov@xored.com>

---
+++ pkg/tools/clientmap/bench_test.go
@@ -0,0 +1,81 @@
+// Copyright (c) 2020 Doc.ai and/or its affiliates.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package clientmap_test
+
+import (
+	"testing"
+
+	"github.com/networkservicemesh/sdk/pkg/networkservice/common/null"
+	"github.com/networkservicemesh/sdk/pkg/tools/clientmap"
+)
+
+var ids = []string{
+	"00", "01", "02", "03", "04", "05", "06", "07", "08", "09",
+	"10", "11", "12", "13", "14", "15", "16", "17", "18", "19",
+	"20", "21", "22", "23", "24", "25", "26", "27", "28", "29",
+	"30", "31", "32", "33", "34", "35", "36", "37", "38", "39",
+	"40", "41", "42", "43", "44", "45", "46", "47", "48", "49",
+	"50", "51", "52", "53", "54", "55", "56", "57", "58", "59",
+	"60", "61", "62", "63", "64", "65", "66", "67", "68", "69",
+	"70", "71", "72", "73", "74", "75", "76", "77", "78", "79",
+	"80", "81", "82", "83", "84", "85", "86", "87", "88", "89",
+	"90", "91", "92", "93", "94", "95", "96", "97", "98", "99", "100",
+}
+
+func BenchmarkMap(b *testing.B) {
+	var m clientmap.Map
+	b.SetParallelism(1000)
+	b.RunParallel(func(pb *testing.PB) {
+		client := null.NewClient()
+		for i := 0; pb.Next(); i++ {
+			id := ids[i%len(ids)]
+			switch i % 6 {
+			case 0:
+				m.Store(id, client)
+			case 1:
+				client, _ = m.LoadOrStore(id, client)
+			case 2:
+				client, _ = m.Load(id)
+			case 3, 4, 5:
+				client, _ = m.LoadAndDelete(id)
+			}
+		}
+	})
+}
+
+func BenchmarkRefcountMap(b *testing.B) {
+	var m clientmap.RefcountMap
+	b.SetParallelism(1000)
+	b.RunParallel(func(pb *testing.PB) {
+		client := null.NewClient()
+		for i := 0; pb.Next(); i++ {
+			id := ids[i%len(ids)]
+			switch i % 8 {
+			case 0:
+				m.Store(id, client)
+			case 1:
+				client, _ = m.LoadOrStore(id, client)
+			case 2:
+				client, _ = m.Load(id)
+			case 3:
+				client, _ = m.LoadUnsafe(id)
+			case 4, 5, 6, 7:
+				client, _ = m.LoadAndDelete(id)
+			}
+		}
+	})
+}
--- pkg/tools/clientmap/refcount_test.go
+++ pkg/tools/clientmap/refcount_test.go
@@ -17,12 +17,19 @@
 package clientmap_test
 
 import (
+	"strconv"
+	"sync"
 	"testing"
 
+	"github.com/networkservicemesh/api/pkg/api/networkservice"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/networkservicemesh/sdk/pkg/networkservice/common/null"
 	"github.com/networkservicemesh/sdk/pkg/tools/clientmap"
+)
+
+const (
+	parallelCount = 1000
 )
 
 func TestRefcountMap(t *testing.T) {
@@ -79,4 +86,161 @@ func CheckRefcount(t *testing.T, m *clientmap.RefcountMap, key string, refcount 
 		client, loaded = m.LoadOrStore(key, client)
 		assert.True(t, loaded || i == 0)
 	}
+}
+
+type sample struct {
+	name      string
+	withValue bool
+	actions   []action
+	validator func(count int, results []bool) bool
+}
+
+var samples = []*sample{
+	{
+		name: "LoadOrStore + LoadOrStore",
+		// 0 1 -> 2 { false, true }
+		// 1 0 -> 2 { true, false }
+		actions: []action{loadOrStore, loadOrStore},
+		validator: func(count int, results []bool) bool {
+			return (count == 2) && !results[0] && results[1] ||
+				(count == 2) && results[0] && !results[1]
+		},
+	},
+	{
+		name: "LoadOrStore + LoadAndDelete",
+		// 0 1 -> 0 { false, true }
+		// 1 0 -> 1 { false, false }
+		actions: []action{loadOrStore, loadAndDelete},
+		validator: func(count int, results []bool) bool {
+			return (count == 0) && !results[0] && results[1] ||
+				(count == 1) && !results[0] && !results[1]
+		},
+	},
+	{
+		name:      "LoadOrStore + LoadAndDelete with value",
+		withValue: true,
+		// 0 1 -> 1 { true, true }
+		// 1 0 -> 1 { false, true }
+		actions: []action{loadOrStore, loadAndDelete},
+		validator: func(count int, results []bool) bool {
+			return (count == 1) && results[0] && results[1] ||
+				(count == 1) && !results[0] && results[1]
+		},
+	},
+	{
+		name:      "LoadAndDelete + LoadAndDelete with value",
+		withValue: true,
+		// 0 1 -> 0 { true, false }
+		// 1 0 -> 0 { false, true }
+		actions: []action{loadAndDelete, loadAndDelete},
+		validator: func(count int, results []bool) bool {
+			return (count == 0) && results[0] && !results[1] ||
+				(count == 0) && !results[0] && results[1]
+		},
+	},
+	{
+		name:      "LoadAndDelete + Load with value",
+		withValue: true,
+		// 0 1 -> 0 { true, false }
+		// 1 0 -> 1 { true, true }
+		actions: []action{loadAndDelete, load},
+		validator: func(count int, results []bool) bool {
+			return (count == 0) && results[0] && !results[1] ||
+				(count == 1) && results[0] && results[1]
+		},
+	},
+	{
+		name:      "LoadAndDelete + LoadAndDelete + Load with value",
+		withValue: true,
+		// 0 1 2 -> 0 { true, false, false }
+		// 0 2 1 -> 0 { true, false, false } (same as ^)
+		// 1 0 2 -> 0 { false, true, false }
+		// 1 2 0 -> 0 { true, true, true }
+		// 2 0 1 -> 0 { false, true, false } (same as ^^)
+		// 2 1 0 -> 0 { true, true, true } (same as ^^)
+		actions: []action{loadAndDelete, loadAndDelete, load},
+		validator: func(count int, results []bool) bool {
+			return (count == 0) && results[0] && !results[1] && !results[2] ||
+				(count == 0) && !results[0] && results[1] && !results[2] ||
+				(count == 0) && results[0] && results[1] && results[2]
+		},
+	},
+	{
+		name:      "LoadOrStore + LoadOrStore + LoadAndDelete with value",
+		withValue: true,
+		// 0 1 2 -> 2 { true, true, true }
+		// 0 2 1 -> 2 { true, true, true } (same as ^)
+		// 1 0 2 -> 2 { true, true, true } (same as ^)
+		// 1 2 0 -> 2 { false, true, true }
+		// 2 0 1 -> 2 { true, true, true } (same as ^^)
+		// 2 1 0 -> 2 { true, false, true }
+		actions: []action{loadOrStore, loadOrStore, loadAndDelete},
+		validator: func(count int, results []bool) bool {
+			return (count == 2) && results[0] && results[1] && results[2] ||
+				(count == 2) && !results[0] && results[1] && results[2] ||
+				(count == 2) && results[0] && !results[1] && results[2]
+		},
+	},
+}
+
+func TestRefcountMap_Actions(t *testing.T) {
+	for _, sample := range samples {
+		runSample(t, new(clientmap.RefcountMap), sample)
+	}
+}
+
+func runSample(t *testing.T, m *clientmap.RefcountMap, sample *sample) {
+	t.Run(sample.name, func(t *testing.T) {
+		value := null.NewClient()
+		for i := 0; i < parallelCount; i++ {
+			key := strconv.Itoa(i)
+			if sample.withValue {
+				m.Store(key, value)
+			}
+
+			results := runActions(m, key, value, sample.actions...)
+
+			var count int
+			for _, loaded := m.LoadAndDelete(key); loaded; _, loaded = m.LoadAndDelete(key) {
+				count++
+			}
+
+			assert.True(t, sample.validator(count, results),
+				"count = %v, results = %v", count, results)
+		}
+	})
+}
+
+type action func(*clientmap.RefcountMap, string, networkservice.NetworkServiceClient) bool
+
+func loadOrStore(m *clientmap.RefcountMap, key string, value networkservice.NetworkServiceClient) bool {
+	_, ok := m.LoadOrStore(key, value)
+	return ok
+}
+
+func load(m *clientmap.RefcountMap, key string, _ networkservice.NetworkServiceClient) bool {
+	_, ok := m.Load(key)
+	return ok
+}
+
+func loadAndDelete(m *clientmap.RefcountMap, key string, _ networkservice.NetworkServiceClient) bool {
+	_, ok := m.LoadAndDelete(key)
+	return ok
+}
+
+func runActions(m *clientmap.RefcountMap, key string, value networkservice.NetworkServiceClient, actions ...action) []bool {
+	wg := new(sync.WaitGroup)
+	wg.Add(len(actions))
+
+	results := make([]bool, len(actions))
+	for i := range actions {
+		go func(k int) {
+			results[k] = actions[k](m, key, value)
+			wg.Done()
+		}(i)
+	}
+
+	wg.Wait()
+
+	return results
 }
