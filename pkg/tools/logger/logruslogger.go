b51735b044a05cfb024cbce08dcb132b78a8b036
Sergey Ershov <sergey.ershov@xored.com>
2020-12-16 12:58:49 +0700

    Refactoring according to golangci-init
    Signed-off-by: Sergey Ershov <sergey.ershov@xored.com>

--- pkg/tools/logger/logruslogger.go
+++ pkg/tools/logger/logruslogger.go
@@ -1,19 +1,40 @@
+// Copyright (c) 2020 Doc.ai and/or its affiliates.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package logger
 
 import (
 	"context"
+	"time"
 
 	"github.com/sirupsen/logrus"
 )
 
-func LogrusLog(ctx context.Context) Logger {
+func newLogrus(ctx context.Context, entry *logrus.Entry) Logger {
+	logger := &logrusLogger{}
+	ctx = context.WithValue(ctx, CTXKEY_LOGGER, logger)
+	logger.entry = entry.WithContext(ctx)
+	return logger
+}
+
+func NewLogrus(ctx context.Context) Logger {
 	if value, ok := ctx.Value(CTXKEY_LOGRUS_ENTRY).(*logrus.Entry); ok {
-		logger := &logrusLogger{}
-		ctx = context.WithValue(ctx, CTXKEY_LOGGER, logger)
-		logger.entry = value.WithContext(ctx)
-		return logger
+		return newLogrus(ctx, value)
 	}
-	return nil
+	return newLogrus(ctx, logrus.WithTime(time.Now()))
 }
 
 type logrusLogger struct {
@@ -52,14 +73,6 @@ func (r *logrusLogger) Fatalf(format string, v ...interface{}) {
 	r.entry.Errorf(format, v...)
 }
 
-func (r *logrusLogger) Trace(v ...interface{}) {
-	r.entry.Error(v...)
-}
-
-func (r *logrusLogger) Tracef(format string, v ...interface{}) {
-	r.entry.Errorf(format, v...)
-}
-
 func (r *logrusLogger) WithField(key, value interface{}) Logger {
 	entry := r.entry
 	entry = entry.WithFields(logrus.Fields{key.(string): value})
@@ -68,4 +81,9 @@ func (r *logrusLogger) WithField(key, value interface{}) Logger {
 	ctx = context.WithValue(ctx, CTXKEY_LOGGER, logger)
 	entry.Context = ctx
 	return logger
+}
+
+//Returns context with logger and logrus entry in it
+func (s *logrusLogger) Context() context.Context {
+	return s.entry.Context
 }
--- pkg/tools/logger/tracelogger.go
+++ pkg/tools/logger/tracelogger.go
@@ -1,9 +1,26 @@
+// Copyright (c) 2020 Doc.ai and/or its affiliates.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package logger
 
 import (
 	"context"
 	"fmt"
 	"strings"
+	"time"
 
 	"github.com/networkservicemesh/sdk/pkg/tools/jaeger"
 	"github.com/opentracing/opentracing-go"
@@ -19,61 +36,57 @@ type traceLogger struct {
 }
 
 func (s *traceLogger) Info(v ...interface{}) {
-	s.Log(INFO, v)
+	s.log(INFO, v)
 }
 
 func (s *traceLogger) Infof(format string, v ...interface{}) {
-	s.Logf(INFO, format, v...)
+	s.logf(INFO, format, v...)
 }
 
 func (s *traceLogger) Warn(v ...interface{}) {
-	s.Log(WARN, v)
+	s.log(WARN, v)
 }
 
 func (s *traceLogger) Warnf(format string, v ...interface{}) {
-	s.Logf(WARN, format, v...)
+	s.logf(WARN, format, v...)
 }
 
 func (s *traceLogger) Error(v ...interface{}) {
-	s.Log(ERROR, v)
+	s.log(ERROR, v)
 }
 
 func (s *traceLogger) Errorf(format string, v ...interface{}) {
-	s.Logf(ERROR, format, v...)
+	s.logf(ERROR, format, v...)
 }
 
 func (s *traceLogger) Fatal(v ...interface{}) {
-	s.Log(FATAL, v)
+	s.log(FATAL, v)
 }
 
 func (s *traceLogger) Fatalf(format string, v ...interface{}) {
-	s.Logf(FATAL, format, v...)
+	s.logf(FATAL, format, v...)
 }
 
-func TraceLog_C(ctx context.Context) Logger {
-	return TraceLog_CO(ctx, SPANLOGGER_OP_UNTITLED)
+func getEntry(ctx context.Context) *logrus.Entry {
+	if value, ok := ctx.Value(CTXKEY_LOGRUS_ENTRY).(*logrus.Entry); ok {
+		return value
+	}
+	return logrus.WithTime(time.Now())
 }
 
-func TraceLog_CO(ctx context.Context, operation string) Logger {
-	var span opentracing.Span
-	if jaeger.IsOpentracingEnabled() {
+func NewTrace(ctx context.Context, operation string, span opentracing.Span) Logger {
+	if jaeger.IsOpentracingEnabled() && span == nil {
 		span, ctx = opentracing.StartSpanFromContext(ctx, operation)
 	}
-	return TraceLog_COS(ctx, operation, span)
-}
-
-func TraceLog_COS(ctx context.Context, operation string, span opentracing.Span) Logger {
 	var info *traceCtxInfo
 	ctx, info = withTraceInfo(ctx)
 	localTraceInfo.Store(info.id, info)
-	if value, ok := ctx.Value(CTXKEY_LOGRUS_ENTRY).(*logrus.Entry); ok {
-		logger := &traceLogger{}
-		ctx = context.WithValue(ctx, CTXKEY_LOGGER, logger)
-		logger.entry = value.WithContext(ctx)
-		logger.printStart(operation)
-		return logger
-	}
-	return nil
+	logger := &traceLogger{span: span, info: info}
+	ctx = context.WithValue(ctx, CTXKEY_LOGGER, logger)
+	logger.entry = getEntry(ctx).WithContext(ctx)
+	logger.ctx = ctx
+	logger.printStart(operation)
+	return logger
 }
 
 func (s *traceLogger) WithField(key, value interface{}) Logger {
@@ -86,13 +99,13 @@ func (s *traceLogger) WithField(key, value interface{}) Logger {
 	return logger
 }
 
-func (s *traceLogger) Log(level loggerLevel, v ...interface{}) {
-	s.Logf(level, format(v), v)
+func (s *traceLogger) log(level loggerLevel, v ...interface{}) {
+	s.logf(level, format(v), v)
 }
 
-func (s *traceLogger) Logf(level loggerLevel, format string, v ...interface{}) {
+func (s *traceLogger) logf(level loggerLevel, format string, v ...interface{}) {
 	msg := fmt.Sprintf(format, v...)
-	s.entry.Tracef("%v %s %s=%v%v", s.info.incInfo(), strings.Repeat(separator, s.info.level), levelName(level), msg, s.getSpan())
+	s.entry.Infof("%v %s %s=%v%v", s.info.incInfo(), strings.Repeat(separator, s.info.level), levelName(level), msg, s.getSpan())
 }
 
 func (s *traceLogger) getSpan() string {
@@ -105,5 +118,20 @@ func (s *traceLogger) getSpan() string {
 
 func (s *traceLogger) printStart(operation string) {
 	prefix := strings.Repeat(startSeparator, s.info.level)
-	s.entry.Tracef("%v%s⎆ %v()%v", s.info.incInfo(), prefix, operation, s.getSpan())
+	s.entry.Infof("%v%s⎆ %v()%v", s.info.incInfo(), prefix, operation, s.getSpan())
+}
+
+//Returns context with logger in it
+func (s *traceLogger) Context() context.Context {
+	return s.ctx
+}
+
+//Returns opentracing span for this logger
+func (s *traceLogger) Span() opentracing.Span {
+	return s.span
+}
+
+//Returns operation name
+func (s *traceLogger) Operation() string {
+	return s.operation
 }
