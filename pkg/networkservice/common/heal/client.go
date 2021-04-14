4dd2d305da031fb9c7db7ee6470b9485e9b9d4e6
Danil Uzlov <DanilUzlov@yandex.ru>
2021-04-14 12:15:00 +0700

    Stop heal client from potentially sending repeating heal requests
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>

--- pkg/networkservice/common/heal/client.go
+++ pkg/networkservice/common/heal/client.go
@@ -31,22 +31,35 @@ import (
 	"github.com/networkservicemesh/sdk/pkg/networkservice/core/next"
 )
 
+type connectionState int
+
+const (
+	connectionstateAwaitingConfirmation connectionState = iota
+	connectionstateReady
+	connectionstateBroken
+)
+
+type connectionInfo struct {
+	conn                  *networkservice.Connection
+	state                 connectionState
+	successVerificationCh chan struct{}
+}
+
 type healClient struct {
-	ctx              context.Context
-	cc               networkservice.MonitorConnectionClient
-	initOnce         sync.Once
-	initErr          error
-	connCache        map[string]*networkservice.Connection
-	conCacheExecutor serialize.Executor
-	expectedConns    verificationRequestMap
+	ctx           context.Context
+	cc            networkservice.MonitorConnectionClient
+	initOnce      sync.Once
+	initErr       error
+	conns         map[string]connectionInfo
+	connsExecutor serialize.Executor
 }
 
 // NewClient - creates a new networkservice.NetworkServiceClient chain element that inform healServer about new client connection
 func NewClient(ctx context.Context, cc networkservice.MonitorConnectionClient) networkservice.NetworkServiceClient {
 	return &healClient{
-		ctx:       ctx,
-		cc:        cc,
-		connCache: make(map[string]*networkservice.Connection),
+		ctx:   ctx,
+		cc:    cc,
+		conns: make(map[string]connectionInfo),
 	}
 }
 
@@ -54,6 +67,9 @@ func (u *healClient) Request(ctx context.Context, request *networkservice.Networ
 	u.initOnce.Do(func() {
 		errCh := make(chan error, 1)
 		pushFunc := healRequestFunc(ctx)
+		if pushFunc == nil {
+			pushFunc = func(id string, restoreConnection bool) {}
+		}
 		go u.listenToConnectionChanges(pushFunc, request.GetConnection().GetCurrentPathSegment(), errCh)
 		u.initErr = <-errCh
 	})
@@ -62,25 +78,26 @@ func (u *healClient) Request(ctx context.Context, request *networkservice.Networ
 		return nil, u.initErr
 	}
 
-	verificationRequest := u.addToExpectedConnections(request.GetConnection())
-	if verificationRequest != nil {
-		defer close(verificationRequest.stopCh)
-	}
+	successVerificationCh := u.addToExpectedConnections(request.GetConnection())
 
 	conn, err := next.Client(ctx).Request(ctx, request, opts...)
 	if err != nil {
 		return nil, err
 	}
 
-	if verificationRequest != nil {
-		err = u.confirmConnectionSuccess(conn, verificationRequest)
+	if successVerificationCh != nil {
+		err = u.confirmConnectionSuccess(conn, successVerificationCh)
 		if err != nil {
+			u.removeConnectionFromMonitor(conn)
 			_, _ = next.Client(ctx).Close(ctx, request.GetConnection(), opts...)
 			return nil, err
 		}
-	} else {
-		u.addConnectionToMonitor(conn)
 	}
+	<-u.connsExecutor.AsyncExec(func() {
+		connInfo := u.conns[conn.GetId()]
+		connInfo.conn = conn.Clone()
+		u.conns[conn.GetId()] = connInfo
+	})
 
 	return conn, nil
 }
@@ -109,13 +126,11 @@ func (u *healClient) listenToConnectionChanges(heal healRequestFuncType, current
 	for {
 		event, err := monitorClient.Recv()
 		if err != nil {
-			if heal != nil {
-				<-u.conCacheExecutor.AsyncExec(func() {
-					for id := range u.connCache {
-						heal(id, true)
-					}
-				})
-			}
+			<-u.connsExecutor.AsyncExec(func() {
+				for id := range u.conns {
+					heal(id, true)
+				}
+			})
 			return
 		}
 
@@ -126,110 +141,71 @@ func (u *healClient) listenToConnectionChanges(heal healRequestFuncType, current
 		// 2. We can't just try to skip first event right after monitor client creation,
 		//    because sometimes we don't get INITIAL_STATE_TRANSFER and therefore we hang indefinitely on Recv().
 		case networkservice.ConnectionEventType_INITIAL_STATE_TRANSFER, networkservice.ConnectionEventType_UPDATE:
-			<-u.conCacheExecutor.AsyncExec(func() {
+			<-u.connsExecutor.AsyncExec(func() {
 				for _, eventConn := range event.GetConnections() {
-					// Here we check if our own connections were updated
-					if conn, ok := u.connCache[eventConn.GetPrevPathSegment().GetId()]; ok {
-						eventConn = eventConn.Clone()
-						if conn.Path == nil {
-							conn.Path = eventConn.Path
-						} else {
-							conn.Path.PathSegments = eventConn.Clone().Path.PathSegments
+					id := eventConn.GetPrevPathSegment().GetId()
+					if connInfo, ok := u.conns[id]; ok {
+						if connInfo.state == connectionstateAwaitingConfirmation {
+							connInfo.successVerificationCh <- struct{}{}
 						}
+						connInfo.state = connectionstateReady
+						connInfo.conn.Path.PathSegments = eventConn.Clone().Path.PathSegments
+						u.conns[id] = connInfo
 					}
 				}
 			})
-
-			u.checkExpectedConnections(event.GetConnections())
-
 		case networkservice.ConnectionEventType_DELETE:
-			if heal != nil {
-				var connsToHeal []string
-				<-u.conCacheExecutor.AsyncExec(func() {
-					for _, eventConn := range event.GetConnections() {
-						// Here we check if something happened to connections that are coming from us, therefore we use GetPrevPathSegment()
-						if conn, ok := u.connCache[eventConn.GetPrevPathSegment().GetId()]; ok {
-							connsToHeal = append(connsToHeal, conn.GetId())
+			<-u.connsExecutor.AsyncExec(func() {
+				for _, eventConn := range event.GetConnections() {
+					id := eventConn.GetPrevPathSegment().GetId()
+					if connInfo, ok := u.conns[id]; ok {
+						if connInfo.state == connectionstateReady {
+							heal(id, false)
 						}
+						connInfo.state = connectionstateBroken
+						u.conns[id] = connInfo
 					}
-				})
-				for _, id := range connsToHeal {
-					heal(id, false)
 				}
-			}
+			})
 		}
 	}
 }
 
-func (u *healClient) checkExpectedConnections(eventConns map[string]*networkservice.Connection) {
-	u.expectedConns.Range(func(reqId string, reqChs connVerificationRequest) bool {
-		for _, conn := range eventConns {
-			if reqId == conn.GetPrevPathSegment().GetId() {
-				select {
-				case <-reqChs.stopCh:
-					u.expectedConns.Delete(reqId)
-					return false
-				case reqChs.connectionSuccessCh <- conn.GetCurrentPathSegment():
-					break
-				}
-			}
-		}
-		return true
-	})
-}
-
-func (u *healClient) addToExpectedConnections(conn *networkservice.Connection) *connVerificationRequest {
-	connAlreadyExists := false
-	<-u.conCacheExecutor.AsyncExec(func() {
-		if cachedConn, ok := u.connCache[conn.GetId()]; ok {
-			if cachedConn.GetId() == conn.GetId() {
-				connAlreadyExists = true
+func (u *healClient) addToExpectedConnections(conn *networkservice.Connection) chan struct{} {
+	var successVerificationCh chan struct{}
+	<-u.connsExecutor.AsyncExec(func() {
+		if _, ok := u.conns[conn.GetId()]; !ok {
+			successVerificationCh = make(chan struct{}, 1)
+			u.conns[conn.GetId()] = connectionInfo{
+				conn:                  conn.Clone(),
+				state:                 connectionstateAwaitingConfirmation,
+				successVerificationCh: successVerificationCh,
 			}
 		}
 	})
 
-	if connAlreadyExists {
-		return nil
-	}
-
-	req := connVerificationRequest{
-		connectionSuccessCh: make(chan *networkservice.PathSegment, 10),
-		stopCh:              make(chan struct{}),
-	}
-
-	u.expectedConns.Store(conn.GetId(), req)
-
-	return &req
+	return successVerificationCh
 }
 
-func (u *healClient) confirmConnectionSuccess(conn *networkservice.Connection, req *connVerificationRequest) error {
+func (u *healClient) confirmConnectionSuccess(conn *networkservice.Connection, successVerificationCh chan struct{}) error {
 	timeoutCh := time.After(time.Millisecond * 100)
-	for {
-		select {
-		case segment := <-req.connectionSuccessCh:
-			if conn.GetNextPathSegment().Equal(segment) {
-				u.addConnectionToMonitor(conn)
-				return nil
-			}
-		case <-timeoutCh:
-			return errors.Errorf("healClient: timeout expired but we couldn't verify that connection was established, connection id: %v", conn.GetId())
-		}
+	select {
+	case <-successVerificationCh:
+		return nil
+	case <-timeoutCh:
+		return errors.Errorf("healClient: timeout expired but we couldn't verify that connection was established, connection id: %v", conn.GetId())
 	}
-}
-
-func (u *healClient) addConnectionToMonitor(conn *networkservice.Connection) {
-	<-u.conCacheExecutor.AsyncExec(func() {
-		u.connCache[conn.GetId()] = conn.Clone()
-	})
 }
 
 func (u *healClient) removeConnectionFromMonitor(conn *networkservice.Connection) (*networkservice.Connection, bool) {
 	var cachedConn *networkservice.Connection
 	var ok bool
-	<-u.conCacheExecutor.AsyncExec(func() {
-		cachedConn, ok = u.connCache[conn.GetId()]
+	<-u.connsExecutor.AsyncExec(func() {
+		var entry connectionInfo
+		entry, ok = u.conns[conn.GetId()]
 		if ok {
-			delete(u.connCache, conn.GetId())
+			cachedConn = entry.conn
+			delete(u.conns, conn.GetId())
 		}
 	})
 
