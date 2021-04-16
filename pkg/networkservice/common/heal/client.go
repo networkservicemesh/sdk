98f3228d7dacbfae7952d1bcdde5d2f16dac1349
Danil Uzlov <DanilUzlov@yandex.ru>
2021-04-16 10:18:37 +0700

    In heal client use mutex per connection instead of one executor
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>

--- pkg/networkservice/common/heal/client.go
+++ pkg/networkservice/common/heal/client.go
@@ -22,7 +22,6 @@ import (
 	"sync"
 	"time"
 
-	"github.com/edwarnicke/serialize"
 	"github.com/golang/protobuf/ptypes/empty"
 	"github.com/networkservicemesh/api/pkg/api/networkservice"
 	"github.com/pkg/errors"
@@ -39,27 +38,19 @@ const (
 	connectionstateBroken
 )
 
-type connectionInfo struct {
-	conn                  *networkservice.Connection
-	state                 connectionState
-	successVerificationCh chan struct{}
-}
-
 type healClient struct {
-	ctx           context.Context
-	cc            networkservice.MonitorConnectionClient
-	initOnce      sync.Once
-	initErr       error
-	conns         map[string]connectionInfo
-	connsExecutor serialize.Executor
+	ctx      context.Context
+	cc       networkservice.MonitorConnectionClient
+	initOnce sync.Once
+	initErr  error
+	conns    connectionInfoMap
 }
 
 // NewClient - creates a new networkservice.NetworkServiceClient chain element that inform healServer about new client connection
 func NewClient(ctx context.Context, cc networkservice.MonitorConnectionClient) networkservice.NetworkServiceClient {
 	return &healClient{
-		ctx:   ctx,
-		cc:    cc,
-		conns: make(map[string]connectionInfo),
+		ctx: ctx,
+		cc:  cc,
 	}
 }
 
@@ -79,19 +70,17 @@ func (u *healClient) Request(ctx context.Context, request *networkservice.Networ
 	}
 
 	var successVerificationCh chan struct{}
-	<-u.connsExecutor.AsyncExec(func() {
+	u.conns.applyLockedOrNew(request.GetConnection().GetId(), func(created bool, info *connectionInfo) {
 		conn := request.GetConnection()
-		if connInfo, ok := u.conns[conn.GetId()]; !ok {
+		if created {
 			successVerificationCh = make(chan struct{}, 1)
-			u.conns[conn.GetId()] = connectionInfo{
-				conn:                  conn.Clone(),
-				state:                 connectionstateAwaitingConfirmation,
-				successVerificationCh: successVerificationCh,
-			}
-			_ = connInfo.conn
-		} else if conn.GetPath() != nil && int(conn.GetPath().Index) < len(conn.GetPath().GetPathSegments())-1 {
-			storedConn := connInfo.conn
-			path := conn.GetPath()
+
+			info.conn = conn.Clone()
+			info.state = connectionstateAwaitingConfirmation
+			info.successVerificationCh = successVerificationCh
+		} else if conn.Path != nil && int(conn.Path.Index) < len(conn.Path.PathSegments)-1 {
+			storedConn := info.conn
+			path := request.GetConnection().Path
 			path.PathSegments = path.PathSegments[:path.Index+1]
 			path.PathSegments = append(path.PathSegments, storedConn.Path.PathSegments[path.Index+1:]...)
 			conn.NetworkServiceEndpointName = storedConn.NetworkServiceEndpointName
@@ -111,10 +100,8 @@ func (u *healClient) Request(ctx context.Context, request *networkservice.Networ
 			return nil, err
 		}
 	}
-	<-u.connsExecutor.AsyncExec(func() {
-		connInfo := u.conns[conn.GetId()]
-		connInfo.conn = conn.Clone()
-		u.conns[conn.GetId()] = connInfo
+	u.conns.applyLocked(conn.GetId(), func(info *connectionInfo) {
+		info.conn = conn.Clone()
 	})
 
 	return conn, nil
@@ -144,45 +131,33 @@ func (u *healClient) listenToConnectionChanges(heal healRequestFuncType, current
 	for {
 		event, err := monitorClient.Recv()
 		if err != nil {
-			<-u.connsExecutor.AsyncExec(func() {
-				for _, connInfo := range u.conns {
-					heal(connInfo.conn, true)
-				}
+			u.conns.Range(func(id string, info *connectionInfo) bool {
+				info.mut.Lock()
+				defer info.mut.Unlock()
+				heal(info.conn, true)
+				return true
 			})
 			return
 		}
 
-		switch event.GetType() {
-		// Why both INITIAL_STATE_TRANSFER and UPDATE:
-		// 1. Sometimes we start polling events too late, and when we wait for confirmation of success of some connection,
-		//    this connection is in the INITIAL_STATE_TRANSFER event, so we must treat these events the same as UPDATE.
-		// 2. We can't just try to skip first event right after monitor client creation,
-		//    because sometimes we don't get INITIAL_STATE_TRANSFER and therefore we hang indefinitely on Recv().
-		case networkservice.ConnectionEventType_INITIAL_STATE_TRANSFER, networkservice.ConnectionEventType_UPDATE:
-			<-u.connsExecutor.AsyncExec(func() {
-				for _, eventConn := range event.GetConnections() {
-					id := eventConn.GetPrevPathSegment().GetId()
-					if connInfo, ok := u.conns[id]; ok {
-						if connInfo.state == connectionstateAwaitingConfirmation {
-							connInfo.successVerificationCh <- struct{}{}
-						}
-						connInfo.state = connectionstateReady
-						connInfo.conn.Path.PathSegments = eventConn.Clone().Path.PathSegments
-						u.conns[id] = connInfo
+		for _, eventConn := range event.GetConnections() {
+			id := eventConn.GetPrevPathSegment().GetId()
+			u.conns.applyLocked(id, func(info *connectionInfo) {
+				switch event.GetType() {
+				// Why both INITIAL_STATE_TRANSFER and UPDATE:
+				// Sometimes we start polling events too late, and when we wait for confirmation of success of some connection,
+				// this connection is in the INITIAL_STATE_TRANSFER event, so we must treat these events the same as UPDATE.
+				case networkservice.ConnectionEventType_INITIAL_STATE_TRANSFER, networkservice.ConnectionEventType_UPDATE:
+					if info.state == connectionstateAwaitingConfirmation {
+						info.successVerificationCh <- struct{}{}
 					}
-				}
-			})
-		case networkservice.ConnectionEventType_DELETE:
-			<-u.connsExecutor.AsyncExec(func() {
-				for _, eventConn := range event.GetConnections() {
-					id := eventConn.GetPrevPathSegment().GetId()
-					if connInfo, ok := u.conns[id]; ok {
-						if connInfo.state == connectionstateReady {
-							heal(connInfo.conn, false)
-						}
-						connInfo.state = connectionstateBroken
-						u.conns[id] = connInfo
+					info.state = connectionstateReady
+					info.conn.Path.PathSegments = eventConn.Clone().Path.PathSegments
+				case networkservice.ConnectionEventType_DELETE:
+					if info.state == connectionstateReady {
+						heal(info.conn, false)
 					}
+					info.state = connectionstateBroken
 				}
 			})
 		}
@@ -200,16 +175,34 @@ func (u *healClient) confirmConnectionSuccess(conn *networkservice.Connection, s
 }
 
 func (u *healClient) removeConnectionFromMonitor(conn *networkservice.Connection) (*networkservice.Connection, bool) {
-	var cachedConn *networkservice.Connection
-	var ok bool
-	<-u.connsExecutor.AsyncExec(func() {
-		var entry connectionInfo
-		entry, ok = u.conns[conn.GetId()]
-		if ok {
-			cachedConn = entry.conn
-			delete(u.conns, conn.GetId())
-		}
-	})
+	info, loaded := u.conns.LoadAndDelete(conn.GetId())
+	if !loaded {
+		return nil, false
+	}
+	info.mut.Lock()
+	defer info.mut.Unlock()
+	return info.conn, true
+}
 
-	return cachedConn, ok
+func (m *connectionInfoMap) applyLocked(id string, fun func(info *connectionInfo)) {
+	info, ok := m.Load(id)
+	if !ok {
+		return
+	}
+	info.mut.Lock()
+	defer info.mut.Unlock()
+	fun(info)
+}
+
+func (m *connectionInfoMap) applyLockedOrNew(id string, fun func(created bool, info *connectionInfo)) {
+	newInfo := &connectionInfo{}
+	// we need to lock this before storing in map to prevent potential race with other threads that can read this map
+	newInfo.mut.Lock()
+	defer newInfo.mut.Unlock()
+	info, loaded := m.LoadOrStore(id, newInfo)
+	if loaded {
+		info.mut.Lock()
+		defer info.mut.Unlock()
+	}
+	fun(!loaded, info)
 }
