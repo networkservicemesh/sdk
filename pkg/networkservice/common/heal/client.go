7bbe7520afa3f58a5d089d3c042ebb1e35174e29
Danil Uzlov <DanilUzlov@yandex.ru>
2021-04-20 10:41:37 +0700

    use sync.Cond instead of channel to confirm connection in heal client
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>

--- pkg/networkservice/common/heal/client.go
+++ pkg/networkservice/common/heal/client.go
@@ -39,10 +39,9 @@ const (
 )
 
 type connectionInfo struct {
-	mut                   sync.Mutex
-	conn                  *networkservice.Connection
-	state                 connState
-	successVerificationCh chan struct{}
+	cond  *sync.Cond
+	conn  *networkservice.Connection
+	state connState
 }
 
 type healClient struct {
@@ -88,53 +87,48 @@ func (u *healClient) Request(ctx context.Context, request *networkservice.Networ
 		return nil, u.initErr
 	}
 
-	var successVerificationCh chan struct{}
-	closeOnError := false
-	u.conns.applyLockedOrNew(request.GetConnection().GetId(), func(created bool, info *connectionInfo) {
-		if created {
-			successVerificationCh = make(chan struct{}, 1)
-
-			info.conn = conn.Clone()
-			info.state = connStateInitial
-			info.successVerificationCh = successVerificationCh
-			closeOnError = true
-		} else if conn.Path != nil && int(conn.Path.Index) < len(conn.Path.PathSegments)-1 {
-			u.replaceConnectionPath(conn, info.conn)
-			if info.state == connStateBroken {
-				successVerificationCh = make(chan struct{}, 1)
-				info.successVerificationCh = successVerificationCh
-			}
-		}
+	connInfo, _ := u.conns.LoadOrStore(conn.GetId(), &connectionInfo{
+		conn: conn.Clone(),
+		cond: sync.NewCond(&sync.Mutex{}),
 	})
+	u.replaceConnectionPath(conn, connInfo)
 
 	conn, err := next.Client(ctx).Request(ctx, request, opts...)
 	if err != nil {
 		return nil, err
 	}
 
-	if successVerificationCh != nil {
-		err = u.confirmConnectionSuccess(conn, successVerificationCh)
-		if err != nil {
-			if closeOnError {
-				u.removeConnectionFromMonitor(conn)
-				_, _ = next.Client(ctx).Close(ctx, request.GetConnection(), opts...)
+	connInfo.cond.L.Lock()
+	defer connInfo.cond.L.Unlock()
+	connInfo.conn = conn.Clone()
+	if connInfo.state != connStateReady {
+		ch := make(chan struct{})
+		go func() {
+			connInfo.cond.Wait()
+			close(ch)
+		}()
+		select {
+		case <-ch:
+		case <-time.After(100 * time.Millisecond):
+			connInfo.cond.Broadcast()
+			<-ch
+			if connInfo.state == connStateInitial {
+				u.conns.Delete(conn.GetId())
 			}
-			return nil, err
+			_, _ = next.Client(ctx).Close(ctx, conn, opts...)
+			return nil, errors.Errorf("healClient: timeout expired but we couldn't verify that connection was established, connection id: %v", conn.GetId())
 		}
 	}
-	u.conns.applyLocked(conn.GetId(), func(info *connectionInfo) {
-		info.conn = conn.Clone()
-	})
 
 	return conn, nil
 }
 
 func (u *healClient) Close(ctx context.Context, conn *networkservice.Connection, opts ...grpc.CallOption) (*empty.Empty, error) {
-	if cachedConn, ok := u.removeConnectionFromMonitor(conn); ok {
-		conn = cachedConn
-	} else {
+	connInfo, loaded := u.conns.LoadAndDelete(conn.GetId())
+	if !loaded {
 		return &empty.Empty{}, nil
 	}
+	u.replaceConnectionPath(conn, connInfo)
 
 	return next.Client(ctx).Close(ctx, conn, opts...)
 }
@@ -147,8 +141,8 @@ func (u *healClient) listenToConnectionChanges(heal requestHealFuncType, monitor
 		event, err := monitorClient.Recv()
 		if err != nil {
 			u.conns.Range(func(id string, info *connectionInfo) bool {
-				info.mut.Lock()
-				defer info.mut.Unlock()
+				info.cond.L.Lock()
+				defer info.cond.L.Unlock()
 				heal(info.conn, true)
 				return true
 			})
@@ -156,85 +150,39 @@ func (u *healClient) listenToConnectionChanges(heal requestHealFuncType, monitor
 		}
 
 		for _, eventConn := range event.GetConnections() {
-			eventConn := eventConn
-			id := eventConn.GetPrevPathSegment().GetId()
-			u.conns.applyLocked(id, func(info *connectionInfo) {
-				switch event.GetType() {
-				// Why both INITIAL_STATE_TRANSFER and UPDATE:
-				// Sometimes we start polling events too late, and when we wait for confirmation of success of some connection,
-				// this connection is in the INITIAL_STATE_TRANSFER event, so we must treat these events the same as UPDATE.
-				case networkservice.ConnectionEventType_INITIAL_STATE_TRANSFER, networkservice.ConnectionEventType_UPDATE:
-					if info.successVerificationCh != nil {
-						close(info.successVerificationCh)
-						info.successVerificationCh = nil
-					}
-					info.state = connStateReady
-					info.conn.Path.PathSegments = eventConn.Clone().Path.PathSegments
-					info.conn.NetworkServiceEndpointName = eventConn.NetworkServiceEndpointName
-				case networkservice.ConnectionEventType_DELETE:
-					if info.state == connStateReady {
-						heal(info.conn, false)
-					}
-					info.state = connStateBroken
+			connID := eventConn.GetPrevPathSegment().GetId()
+			connInfo, ok := u.conns.Load(connID)
+			if !ok {
+				continue
+			}
+			connInfo.cond.L.Lock()
+			switch event.GetType() {
+			// Why both INITIAL_STATE_TRANSFER and UPDATE:
+			// Sometimes we start polling events too late, and when we wait for confirmation of success of some connection,
+			// this connection is in the INITIAL_STATE_TRANSFER event, so we must treat these events the same as UPDATE.
+			case networkservice.ConnectionEventType_INITIAL_STATE_TRANSFER, networkservice.ConnectionEventType_UPDATE:
+				connInfo.state = connStateReady
+				connInfo.conn.Path.PathSegments = eventConn.Clone().Path.PathSegments
+				connInfo.conn.NetworkServiceEndpointName = eventConn.NetworkServiceEndpointName
+				connInfo.cond.Broadcast()
+			case networkservice.ConnectionEventType_DELETE:
+				if connInfo.state == connStateReady {
+					heal(connInfo.conn, false)
 				}
-			})
+				connInfo.state = connStateBroken
+			}
+			connInfo.cond.L.Unlock()
 		}
 	}
 }
 
-// confirmConnectionSuccess - waits for monitor thread to receive event confirming successful connection to next node
-func (u *healClient) confirmConnectionSuccess(conn *networkservice.Connection, successVerificationCh chan struct{}) error {
-	timeoutCh := time.After(time.Millisecond * 100)
-	select {
-	case <-successVerificationCh:
-		return nil
-	case <-timeoutCh:
-		return errors.Errorf("healClient: timeout expired but we couldn't verify that connection was established, connection id: %v", conn.GetId())
-	}
-}
-
-func (u *healClient) removeConnectionFromMonitor(conn *networkservice.Connection) (*networkservice.Connection, bool) {
-	info, loaded := u.conns.LoadAndDelete(conn.GetId())
-	if !loaded {
-		return nil, false
-	}
-	info.mut.Lock()
-	defer info.mut.Unlock()
-	return info.conn, true
-}
-
-// applyLocked - searches the map for entry with key=id and runs provided function while entry mutex is locked.
-//               If map doesn't contain this key, does nothing.
-func (m *connectionInfoMap) applyLocked(id string, fun func(info *connectionInfo)) {
-	info, ok := m.Load(id)
-	if !ok {
-		return
-	}
-	info.mut.Lock()
-	defer info.mut.Unlock()
-	fun(info)
-}
-
-// applyLocked - searches the map for entry with key=id and runs provided function while entry mutex is locked.
-//               If map doesn't have entry with this key creates a new entry.
-//               Function is informed whether it receives new key or already existing key via first argument.
-func (m *connectionInfoMap) applyLockedOrNew(id string, fun func(created bool, info *connectionInfo)) {
-	newInfo := &connectionInfo{}
-	// we need to lock this before storing in map to prevent potential race with other threads that can read this map
-	newInfo.mut.Lock()
-	defer newInfo.mut.Unlock()
-	info, loaded := m.LoadOrStore(id, newInfo)
-	if loaded {
-		info.mut.Lock()
-		defer info.mut.Unlock()
-	}
-	fun(!loaded, info)
-}
-func (u *healClient) replaceConnectionPath(conn, storedConn *networkservice.Connection) {
+func (u *healClient) replaceConnectionPath(conn *networkservice.Connection, connInfo *connectionInfo) {
 	path := conn.GetPath()
 	if path != nil && int(path.Index) < len(path.PathSegments)-1 {
+		connInfo.cond.L.Lock()
+		defer connInfo.cond.L.Unlock()
 		path.PathSegments = path.PathSegments[:path.Index+1]
-		path.PathSegments = append(path.PathSegments, storedConn.Path.PathSegments[path.Index+1:]...)
-		conn.NetworkServiceEndpointName = storedConn.NetworkServiceEndpointName
+		path.PathSegments = append(path.PathSegments, connInfo.conn.Path.PathSegments[path.Index+1:]...)
+		conn.NetworkServiceEndpointName = connInfo.conn.NetworkServiceEndpointName
 	}
 }
