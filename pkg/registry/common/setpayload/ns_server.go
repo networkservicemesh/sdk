e0c0ec818ceb5e30e3a62e8ea2953e37c2304f60
Авраменко Михаил <avramenkomihail15@gmail.com>
2021-04-22 12:14:30 +0700

    issue#792 made payload "Ip" by default (#832)
    * Update go.mod and go.sum to latest version from networkservicemesh/ap…
    
    Signed-off-by: Mikhail <avramenkomihail15@gmail.com>
    
    * using IP as fallback for empty payload
    
    Signed-off-by: Mikhail <avramenkomihail15@gmail.com>
    
    * Generate nse.Name on the Endpoint side (#789)
    
    * Rework setid
    
    Signed-off-by: Vladimir Popov <vladimir.popov@xored.com>
    
    * Rework everything to new setid
    
    Signed-off-by: Vladimir Popov <vladimir.popov@xored.com>
    
    * Fix localbypass
    
    Signed-off-by: Vladimir Popov <vladimir.popov@xored.com>
    
    * Move NSE setid.NewServer() -> checkid.NewServer()
    
    Signed-off-by: Vladimir Popov <vladimir.popov@xored.com>
    
    * Fix TestNSMGR_LocalUsecaseNoURL
    
    Signed-off-by: Vladimir Popov <vladimir.popov@xored.com>
    
    * Move checkid to common, rework to stringurl.Map
    
    Signed-off-by: Vladimir Popov <vladimir.popov@xored.com>
    
    * Replace DuplicateError with status.Error(codes.AlreadyExists)
    
    Signed-off-by: Vladimir Popov <vladimir.popov@xored.com>
    
    * style fix
    
    Signed-off-by: Mikhail <avramenkomihail15@gmail.com>
    
    * header style fix
    
    Signed-off-by: Mikhail <avramenkomihail15@gmail.com>
    
    * import style fix
    
    Signed-off-by: Mikhail <avramenkomihail15@gmail.com>
    
    * removed unused test case, small header fix
    
    Signed-off-by: Mikhail <avramenkomihail15@gmail.com>
    
    * fixed setting payload sandbox test
    
    Signed-off-by: Mikhail <avramenkomihail15@gmail.com>
    
    * code format fix
    
    Signed-off-by: Mikhail <avramenkomihail15@gmail.com>
    
    * code format fix 2
    
    Signed-off-by: Mikhail <avramenkomihail15@gmail.com>
    
    * last style fix
    
    Signed-off-by: Mikhail <avramenkomihail15@gmail.com>
    
    * applied suggested changes
    
    Signed-off-by: Mikhail <avramenkomihail15@gmail.com>
    
    * quick fix
    
    Signed-off-by: Mikhail <avramenkomihail15@gmail.com>
    
    * import fix
    
    Signed-off-by: Mikhail <avramenkomihail15@gmail.com>
    
    * fixed formatting
    
    Signed-off-by: Mikhail <avramenkomihail15@gmail.com>
    
    * skipped ustable test
    
    Signed-off-by: Mikhail <avramenkomihail15@gmail.com>
    
    * fixed header pattern
    
    Signed-off-by: Mikhail <avramenkomihail15@gmail.com>
    
    * applied suggested changes
    
    Signed-off-by: Mikhail <avramenkomihail15@gmail.com>
    
    * fast fix
    
    Signed-off-by: Mikhail <avramenkomihail15@gmail.com>
    
    * test fix
    
    Signed-off-by: Mikhail <avramenkomihail15@gmail.com>
    
    * used options pattern for payload
    
    Signed-off-by: Mikhail <avramenkomihail15@gmail.com>
    
    * format fix
    
    Signed-off-by: Mikhail <avramenkomihail15@gmail.com>
    
    * format fix 2
    
    Signed-off-by: Mikhail <avramenkomihail15@gmail.com>
    
    * changed configuration options
    
    Signed-off-by: Mikhail <avramenkomihail15@gmail.com>
    
    Co-authored-by: Network Service Mesh Bot <60070799+nsmbot@users.noreply.github.com>
    Co-authored-by: Vladimir Popov <vladimir.popov@xored.com>

---
+++ pkg/registry/common/setpayload/ns_server.go
@@ -0,0 +1,58 @@
+// Copyright (c) 2021 Doc.ai and/or its affiliates.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package setpayload
+
+import (
+	"context"
+
+	"github.com/golang/protobuf/ptypes/empty"
+	"github.com/networkservicemesh/api/pkg/api/networkservice/payload"
+	"github.com/networkservicemesh/api/pkg/api/registry"
+
+	"github.com/networkservicemesh/sdk/pkg/registry/core/next"
+)
+
+type setPayloadNSServer struct {
+	defaultPayload string
+}
+
+func (s *setPayloadNSServer) Register(ctx context.Context, ns *registry.NetworkService) (*registry.NetworkService, error) {
+	if ns.Payload == "" {
+		ns.Payload = s.defaultPayload
+	}
+
+	return next.NetworkServiceRegistryServer(ctx).Register(ctx, ns)
+}
+
+func (s *setPayloadNSServer) Find(query *registry.NetworkServiceQuery, server registry.NetworkServiceRegistry_FindServer) error {
+	return next.NetworkServiceRegistryServer(server.Context()).Find(query, server)
+}
+
+func (s *setPayloadNSServer) Unregister(ctx context.Context, ns *registry.NetworkService) (*empty.Empty, error) {
+	return next.NetworkServiceRegistryServer(ctx).Unregister(ctx, ns)
+}
+
+// NewNetworkServiceRegistryServer creates new instance of NetworkServiceRegistryServer which sets the passed options
+func NewNetworkServiceRegistryServer(options ...Option) registry.NetworkServiceRegistryServer {
+	payloadServer := &setPayloadNSServer{defaultPayload: payload.IP}
+
+	for _, o := range options {
+		o(payloadServer)
+	}
+
+	return payloadServer
+}
