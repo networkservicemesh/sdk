19c7a102f2a7bb9914763ec77304a69ce7790f0e
Uzlov Danil <36223296+d-uzlov@users.noreply.github.com>
2021-04-22 12:47:30 +0700

    Add Client Monitor to the Heal Client (#810)
    * Fix selector handling in monitorServer.MonitorConnections
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * Move event monitoring from heal server to heal client
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * Fix heal not working in sandbox tests
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * Fix pending issues and clean code in heal package
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * Fix heal client not working reliably on first request
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * Fix issues, discussed in PR comments
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * Fix heal unit test panic
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * Fix imports layout
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * Fix heal client using wrong id when listening to connection updates
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * Stop heal client from potentially sending repeating heal requests
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * Use updated connection path on heal
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * Move connection path restoring from heal server to heal client
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * In heal client use mutex per connection instead of one executor
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * In heal server use mutex per connection instead of one executor
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * Fix linter warning
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * Add comments to "heal" package
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * Fix heal client not saving endpoint name on connection update events
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * Make heal client wait for confirmation in Request if connection was broken
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * extract initialization method in heal client
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * use sync.Cond instead of channel to confirm connection in heal client
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * use separate functions for "heal" and "restore connection"
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * use better error message in heal client
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * fix file formatting
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * refactor heal server code
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * use single type for both heal requests
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * combine types in healClient.listenToConnectionChanges()
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * remove event-check in heal client, remove incorrect heal test
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * remove complex state from conn info in heal client
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * use mutex instead of sync.Cond in heal client
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * fix linter warning
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * fix heal client not removing unsuccessful connection
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * fix heal server not using clock package during healing
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>

--- pkg/networkservice/common/heal/client.go
+++ pkg/networkservice/common/heal/client.go
@@ -19,24 +19,34 @@ package heal
 
 import (
 	"context"
+	"sync"
 
 	"github.com/golang/protobuf/ptypes/empty"
-	"google.golang.org/grpc"
-
 	"github.com/networkservicemesh/api/pkg/api/networkservice"
+	"github.com/pkg/errors"
+	"google.golang.org/grpc"
 
 	"github.com/networkservicemesh/sdk/pkg/networkservice/core/next"
 )
 
-// RegisterClientFunc - required to inform heal server about new client connection and assign it to the connection ID
-type RegisterClientFunc func(context.Context, *networkservice.Connection, networkservice.MonitorConnectionClient)
-
-type healClient struct {
-	ctx context.Context
-	cc  networkservice.MonitorConnectionClient
+type connectionInfo struct {
+	mut    sync.Mutex
+	conn   *networkservice.Connection
+	active bool
 }
 
-// NewClient - creates a new networkservice.NetworkServiceClient chain element that inform healServer about new client connection
+type healClient struct {
+	ctx      context.Context
+	cc       networkservice.MonitorConnectionClient
+	initOnce sync.Once
+	initErr  error
+	conns    connectionInfoMap
+}
+
+// NewClient - creates a new networkservice.NetworkServiceClient chain element that monitors its connections' state
+//             and calls heal server in case connection breaks if heal server is present in the chain
+//             - ctx - context for the lifecycle of the *Client* itself.  Cancel when discarding the client.
+//             - cc  - MonitorConnectionClient that will be used to watch for connection confirmations and breakages.
 func NewClient(ctx context.Context, cc networkservice.MonitorConnectionClient) networkservice.NetworkServiceClient {
 	return &healClient{
 		ctx: ctx,
@@ -44,16 +54,118 @@ func NewClient(ctx context.Context, cc networkservice.MonitorConnectionClient) n
 	}
 }
 
-func (u *healClient) Request(ctx context.Context, request *networkservice.NetworkServiceRequest, opts ...grpc.CallOption) (*networkservice.Connection, error) {
-	conn, err := next.Client(ctx).Request(ctx, request, opts...)
-
-	registerClient := registerClientFunc(ctx)
-	if err == nil && registerClient != nil {
-		registerClient(u.ctx, conn, u.cc)
+func (u *healClient) init(ctx context.Context, conn *networkservice.Connection) error {
+	monitorClient, err := u.cc.MonitorConnections(u.ctx, &networkservice.MonitorScopeSelector{
+		PathSegments: []*networkservice.PathSegment{{Name: conn.GetCurrentPathSegment().Name}, {Name: ""}},
+	})
+	if err != nil {
+		return errors.Wrap(err, "MonitorConnections failed")
 	}
-	return conn, err
+
+	healConnection := requestHealConnectionFunc(ctx)
+	if healConnection == nil {
+		healConnection = func(conn *networkservice.Connection) {}
+	}
+	restoreConnection := requestRestoreConnectionFunc(ctx)
+	if restoreConnection == nil {
+		restoreConnection = func(conn *networkservice.Connection) {}
+	}
+
+	go u.listenToConnectionChanges(healConnection, restoreConnection, monitorClient)
+
+	return nil
+}
+
+func (u *healClient) Request(ctx context.Context, request *networkservice.NetworkServiceRequest, opts ...grpc.CallOption) (*networkservice.Connection, error) {
+	conn := request.GetConnection()
+	u.initOnce.Do(func() {
+		u.initErr = u.init(ctx, conn)
+	})
+	// if initialization failed, then we want for all subsequent calls to Request() on this object to also fail
+	if u.initErr != nil {
+		return nil, u.initErr
+	}
+
+	connInfo, loaded := u.conns.LoadOrStore(conn.GetId(), &connectionInfo{
+		conn: conn.Clone(),
+		mut:  sync.Mutex{},
+	})
+	u.replaceConnectionPath(conn, connInfo)
+
+	conn, err := next.Client(ctx).Request(ctx, request, opts...)
+	if err != nil {
+		if !loaded {
+			u.conns.Delete(request.GetConnection().GetId())
+		}
+		return nil, err
+	}
+
+	connInfo.mut.Lock()
+	defer connInfo.mut.Unlock()
+	connInfo.conn = conn.Clone()
+
+	return conn, nil
 }
 
 func (u *healClient) Close(ctx context.Context, conn *networkservice.Connection, opts ...grpc.CallOption) (*empty.Empty, error) {
+	connInfo, loaded := u.conns.LoadAndDelete(conn.GetId())
+	if !loaded {
+		return &empty.Empty{}, nil
+	}
+	u.replaceConnectionPath(conn, connInfo)
+
 	return next.Client(ctx).Close(ctx, conn, opts...)
+}
+
+// listenToConnectionChanges - loops on events from MonitorConnectionClient while the monitor client is alive.
+//                             Updates connection cache and broadcasts events of successful connections.
+//                             Calls heal when something breaks.
+func (u *healClient) listenToConnectionChanges(healConnection, restoreConnection requestHealFuncType, monitorClient networkservice.MonitorConnection_MonitorConnectionsClient) {
+	for {
+		event, err := monitorClient.Recv()
+		if err != nil {
+			u.conns.Range(func(id string, info *connectionInfo) bool {
+				info.mut.Lock()
+				defer info.mut.Unlock()
+				restoreConnection(info.conn)
+				return true
+			})
+			return
+		}
+
+		for _, eventConn := range event.GetConnections() {
+			connID := eventConn.GetPrevPathSegment().GetId()
+			connInfo, ok := u.conns.Load(connID)
+			if !ok {
+				continue
+			}
+			connInfo.mut.Lock()
+			switch event.GetType() {
+			// Why both INITIAL_STATE_TRANSFER and UPDATE:
+			// Sometimes we start polling events too late, and when we wait for confirmation of success of some connection,
+			// this connection is in the INITIAL_STATE_TRANSFER event, so we must treat these events the same as UPDATE.
+			case networkservice.ConnectionEventType_INITIAL_STATE_TRANSFER, networkservice.ConnectionEventType_UPDATE:
+				connInfo.active = true
+				connInfo.conn.Path.PathSegments = eventConn.Clone().Path.PathSegments
+				connInfo.conn.NetworkServiceEndpointName = eventConn.NetworkServiceEndpointName
+			case networkservice.ConnectionEventType_DELETE:
+				if connInfo.active {
+					healConnection(connInfo.conn)
+				}
+				connInfo.active = false
+			}
+			connInfo.mut.Unlock()
+		}
+	}
+}
+
+func (u *healClient) replaceConnectionPath(conn *networkservice.Connection, connInfo *connectionInfo) {
+	path := conn.GetPath()
+	if path != nil && int(path.Index) < len(path.PathSegments)-1 {
+		connInfo.mut.Lock()
+		defer connInfo.mut.Unlock()
+		path.PathSegments = path.PathSegments[:path.Index+1]
+		path.PathSegments = append(path.PathSegments, connInfo.conn.Path.PathSegments[path.Index+1:]...)
+		conn.NetworkServiceEndpointName = connInfo.conn.NetworkServiceEndpointName
+	}
 }
