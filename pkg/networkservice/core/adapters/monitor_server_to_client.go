25492cd8c892782f92ba49c8bff5c73f877604e6
Danil Uzlov <DanilUzlov@yandex.ru>
2021-04-23 16:58:19 +0700

    fix copyright notice
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>

--- pkg/networkservice/core/adapters/monitor_server_to_client.go
+++ pkg/networkservice/core/adapters/monitor_server_to_client.go
@@ -1,6 +1,6 @@
-// Copyright (c) 2021 Doc.ai and/or its affiliates.
-//
 // Copyright (c) 2020 Cisco and/or its affiliates.
+//
+// Copyright (c) 2021 Doc.ai and/or its affiliates.
 //
 // SPDX-License-Identifier: Apache-2.0
 //
