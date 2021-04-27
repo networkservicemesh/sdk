8aeaaa34751369901b17f8dbeaa82fbbae6c1015
Danil Uzlov <DanilUzlov@yandex.ru>
2021-04-27 10:28:04 +0700

    add tests for matchutils.MatchNetworkServices
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>

---
+++ pkg/tools/matchutils/utils_test.go
@@ -0,0 +1,140 @@
+// Copyright (c) 2020 Doc.ai and/or its affiliates.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package matchutils_test
+
+import (
+	"testing"
+
+	"github.com/networkservicemesh/api/pkg/api/networkservice/payload"
+	"github.com/networkservicemesh/api/pkg/api/registry"
+	"github.com/stretchr/testify/require"
+
+	"github.com/networkservicemesh/sdk/pkg/tools/matchutils"
+)
+
+func TestNSMatch(t *testing.T) {
+	referenceService := &registry.NetworkService{
+		Name:    "ns-1-substring-match",
+		Payload: payload.IP,
+		Matches: []*registry.Match{
+			{
+				SourceSelector: map[string]string{
+					"app": "firewall",
+				},
+				Routes: []*registry.Destination{
+					{
+						DestinationSelector: map[string]string{
+							"app": "some-middle-app",
+						},
+					},
+				},
+			},
+		},
+	}
+
+	type test struct {
+		name string
+		svc  *registry.NetworkService
+		want bool
+	}
+
+	tests := []test{
+		{
+			name: "empty",
+			svc:  &registry.NetworkService{},
+			want: true,
+		},
+		{
+			name: "same",
+			svc:  referenceService,
+			want: true,
+		},
+		{
+			name: "matchName",
+			svc: &registry.NetworkService{
+				Name: referenceService.Name,
+			},
+			want: true,
+		},
+		{
+			name: "matchNameSubstring",
+			svc: &registry.NetworkService{
+				Name: "substring-match",
+			},
+			want: true,
+		},
+		{
+			name: "noMatchName",
+			svc: &registry.NetworkService{
+				Name: "different-name",
+			},
+			want: false,
+		},
+		{
+			name: "matchPayload",
+			svc: &registry.NetworkService{
+				Payload: referenceService.Payload,
+			},
+			want: true,
+		},
+		{
+			name: "noMatchPayload",
+			svc: &registry.NetworkService{
+				Payload: payload.Ethernet,
+			},
+			want: false,
+		},
+		{
+			name: "matchMatches",
+			svc: &registry.NetworkService{
+				Matches: referenceService.Matches,
+			},
+			want: true,
+		},
+		{
+			name: "noMatchPayload",
+			svc: &registry.NetworkService{
+				Matches: []*registry.Match{
+					{
+						SourceSelector: map[string]string{
+							"app": "vpn",
+						},
+						Routes: []*registry.Destination{
+							{
+								DestinationSelector: map[string]string{
+									"app": "some-middle-app",
+								},
+							},
+						},
+					},
+				},
+			},
+			want: false,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			got := matchutils.MatchNetworkServices(tc.svc, referenceService)
+			if tc.want != got {
+				t.Logf("matching right: %v", referenceService)
+				t.Logf("matching left: %v", tc.svc)
+				require.Equal(t, tc.want, got)
+			}
+		})
+	}
+}
