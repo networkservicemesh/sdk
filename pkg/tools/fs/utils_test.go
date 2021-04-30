ea9caf60f8842dbccc7ac4775329665add5519d4
Danil Uzlov <DanilUzlov@yandex.ru>
2021-04-30 11:59:31 +0700

    test
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>

--- pkg/tools/fs/utils_test.go
+++ pkg/tools/fs/utils_test.go
@@ -73,32 +73,45 @@ func Test_WatchFile(t *testing.T) {
 		return nil
 	}
 
-	require.Nil(t, expectEvent(), filePath)
+	require.Nil(t, expectEvent(), filePath) // initial file read, nil because file doesn't exist
 
 	err = ioutil.WriteFile(filePath, []byte("data"), os.ModePerm)
 	checkPathError(t, err)
-	require.NotNil(t, expectEvent(), filePath)
+	require.NotNil(t, expectEvent(), filePath) // file created
 
 	// https://github.com/fsnotify/fsnotify/issues/11
 	if runtime.GOOS != "darwin" {
-		require.NotNil(t, expectEvent(), filePath)
+		require.NotNil(t, expectEvent(), filePath) // file write. MacOS doesn't support write events
 	}
+	time.Sleep(time.Millisecond * 50)
 
 	err = os.RemoveAll(path)
 	checkPathError(t, err)
 	if runtime.GOOS != "darwin" {
-		require.Nil(t, expectEvent(), filePath)
+		require.Nil(t, expectEvent(), filePath) // file removed
 	} else {
-		for {
-			event := expectEvent()
-			if event == nil {
-				break
-			}
+		event := expectEvent()
+		if event != nil {
+			event = expectEvent()
 		}
+		require.Nil(t, event, filePath) // ...but MacOS sometimes sends some write events. Very rarely, but it happens
 	}
+	//if runtime.GOOS != "darwin" {
+	//} else { // ...but MacOS sometimes sends some write events. Very rarely, but it happens
+	//	for {
+	//		event := expectEvent()
+	//		if event == nil {
+	//			break
+	//		}
+	//	}
+	//}
 
 	err = os.MkdirAll(path, os.ModePerm)
 	for i := 0; i < 5 && err != nil; i++ {
+		// Removing file is async operation.
+		// Waiting for events should theoretically sync us with filesystem,
+		// but apparently sometimes it's not enough for the folder to become available for write.
+		// Particularly, this can be observed on slow Windows OS systems.
 		time.Sleep(time.Millisecond * 50)
 		err = os.MkdirAll(path, os.ModePerm)
 	}
@@ -106,5 +119,5 @@ func Test_WatchFile(t *testing.T) {
 
 	err = ioutil.WriteFile(filePath, []byte("data"), os.ModePerm)
 	checkPathError(t, err)
-	require.NotNil(t, expectEvent(), filePath)
+	require.NotNil(t, expectEvent(), filePath) // file created
 }
