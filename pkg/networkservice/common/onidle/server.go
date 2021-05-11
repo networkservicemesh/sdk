93d252947219d952fd428e98e86e63ea17550579
Uzlov Danil <36223296+d-uzlov@users.noreply.github.com>
2021-05-11 21:22:51 +0700

    Add onidle chain element (#908)
    * add endpointtimeout chain element
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * fix TestEndpointTimeout_ContextCancel instability
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * make endpointTimeoutServer track connections, fix concurrency issues
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * rename package endpointtimeout to idlenotifier
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * fix formatting
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * add HoldingActiveRequest test, clean code
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * fix race in *endpointTimeoutServer)
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * add TestIdleNotifier_FailedRequest
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * fix missing error check
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * make idlenotifier.notify action argument mandatory
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * rename package idlenotifier to onidle
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * fix TestIdleNotifier_ContextCancel instability
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * clean code
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * fix race in onIdleServer
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * fix timer leak
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * use require.Eventually + goleak.Find instead of time.Sleep in TestIdleNotifier_ContextCancel
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * remove excessive removeConnection call
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * use simple map instead of sync.Map in onIdleServer
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * adjuct comments
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * refactor timer handling
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * add TestIdleNotifier_RequestAfterExpire test
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * remove unneeded check from TestIdleNotifier_ContextCancel
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * fix non-american "cancelled" spelling error
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * add extra check for context error
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * increase check timeout from 50ms to 100ms
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * extract notify callback to be not under the mutex
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>
    
    * apply suggestions from code review
    
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>

---
+++ pkg/networkservice/common/onidle/server.go
@@ -0,0 +1,133 @@
+// Copyright (c) 2021 Doc.ai and/or its affiliates.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package onidle provides server chain element that executes a callback when there were no active connections for specified time
+package onidle
+
+import (
+	"context"
+	"sync"
+	"time"
+
+	"github.com/golang/protobuf/ptypes/empty"
+	"github.com/networkservicemesh/api/pkg/api/networkservice"
+	"github.com/pkg/errors"
+
+	"github.com/networkservicemesh/sdk/pkg/networkservice/core/next"
+	"github.com/networkservicemesh/sdk/pkg/tools/clock"
+)
+
+type onIdleServer struct {
+	ctx         context.Context
+	timeout     time.Duration
+	notify      func()
+	timer       clock.Timer
+	timerMut    sync.Mutex
+	timerFired  bool
+	activeConns map[string]struct{}
+}
+
+// NewServer returns a new server chain element that notifies about long time periods without active connections.
+//
+// If timeout passes, server calls specified notify function and all further Requests will fail.
+//
+// If ctx is canceled before timeout, the server stops monitoring connections without calling notify.
+// Further calls to Request will not be affected by this.
+func NewServer(ctx context.Context, notify func(), options ...Option) networkservice.NetworkServiceServer {
+	clockTime := clock.FromContext(ctx)
+
+	t := &onIdleServer{
+		ctx:         ctx,
+		timeout:     time.Minute * 10,
+		notify:      notify,
+		activeConns: make(map[string]struct{}),
+	}
+
+	for _, opt := range options {
+		opt(t)
+	}
+
+	t.timer = clockTime.AfterFunc(t.timeout, func() {
+		if ctx.Err() != nil {
+			return
+		}
+
+		t.timerMut.Lock()
+
+		if t.timerFired || len(t.activeConns) != 0 {
+			t.timerMut.Unlock()
+			return
+		}
+
+		t.timerFired = true
+		t.timerMut.Unlock()
+		t.notify()
+	})
+
+	go func() {
+		<-t.ctx.Done()
+		t.timer.Stop()
+	}()
+
+	return t
+}
+
+func (t *onIdleServer) Request(ctx context.Context, request *networkservice.NetworkServiceRequest) (*networkservice.Connection, error) {
+	isRefresh, expired := t.addConnection(request.GetConnection())
+
+	if expired {
+		return nil, errors.New("endpoint expired")
+	}
+
+	conn, err := next.Server(ctx).Request(ctx, request)
+	if err != nil && !isRefresh {
+		t.removeConnection(request.GetConnection())
+	}
+
+	return conn, err
+}
+
+func (t *onIdleServer) Close(ctx context.Context, conn *networkservice.Connection) (*empty.Empty, error) {
+	t.removeConnection(conn)
+	return next.Server(ctx).Close(ctx, conn)
+}
+
+func (t *onIdleServer) addConnection(conn *networkservice.Connection) (isRefresh, expired bool) {
+	t.timerMut.Lock()
+	defer t.timerMut.Unlock()
+
+	if t.timerFired {
+		return false, true
+	}
+
+	if _, isRefresh = t.activeConns[conn.GetId()]; !isRefresh {
+		t.activeConns[conn.GetId()] = struct{}{}
+		t.timer.Stop()
+	}
+	return isRefresh, false
+}
+
+func (t *onIdleServer) removeConnection(conn *networkservice.Connection) {
+	t.timerMut.Lock()
+	defer t.timerMut.Unlock()
+
+	if _, loaded := t.activeConns[conn.GetId()]; loaded {
+		delete(t.activeConns, conn.GetId())
+		if len(t.activeConns) == 0 && t.ctx.Err() == nil {
+			t.timer.Reset(t.timeout)
+		}
+	}
+}
---
+++ pkg/networkservice/common/onidle/server_test.go
@@ -0,0 +1,209 @@
+// Copyright (c) 2021 Doc.ai and/or its affiliates.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package onidle_test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/networkservicemesh/api/pkg/api/networkservice"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/atomic"
+	"go.uber.org/goleak"
+
+	"github.com/networkservicemesh/sdk/pkg/networkservice/common/onidle"
+	"github.com/networkservicemesh/sdk/pkg/networkservice/core/next"
+	"github.com/networkservicemesh/sdk/pkg/networkservice/utils/inject/injecterror"
+	"github.com/networkservicemesh/sdk/pkg/tools/clock"
+	"github.com/networkservicemesh/sdk/pkg/tools/clockmock"
+)
+
+const (
+	testWait = 100 * time.Millisecond
+	testTick = testWait / 100
+)
+
+func TestIdleNotifier_NoRequests(t *testing.T) {
+	t.Cleanup(func() { goleak.VerifyNone(t) })
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	clockMock := clockmock.New()
+	ctx = clock.WithClock(ctx, clockMock)
+
+	timeout := time.Hour
+	var flag atomic.Bool
+
+	_ = onidle.NewServer(ctx, func() {
+		flag.Store(true)
+	}, onidle.WithTimeout(timeout))
+
+	clockMock.Add(timeout - 1)
+	require.Never(t, flag.Load, testWait, testTick)
+
+	clockMock.Add(1)
+	require.Eventually(t, flag.Load, testWait, testTick)
+}
+
+func TestIdleNotifier_Refresh(t *testing.T) {
+	t.Cleanup(func() { goleak.VerifyNone(t) })
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	clockMock := clockmock.New()
+	ctx = clock.WithClock(ctx, clockMock)
+
+	timeout := time.Hour
+	var flag atomic.Bool
+
+	server := onidle.NewServer(ctx, func() {
+		flag.Store(true)
+	}, onidle.WithTimeout(timeout))
+
+	clockMock.Add(timeout - 1)
+	conn, err := server.Request(ctx, &networkservice.NetworkServiceRequest{})
+	require.NoError(t, err)
+	clockMock.Add(timeout)
+	require.Never(t, flag.Load, testWait, testTick)
+
+	_, err = server.Close(ctx, conn)
+	require.NoError(t, err)
+	clockMock.Add(timeout - 1)
+	require.Never(t, flag.Load, testWait, testTick)
+	clockMock.Add(1)
+	require.Eventually(t, flag.Load, testWait, testTick)
+}
+
+func TestIdleNotifier_HoldingActiveRequest(t *testing.T) {
+	t.Cleanup(func() { goleak.VerifyNone(t) })
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	clockMock := clockmock.New()
+	ctx = clock.WithClock(ctx, clockMock)
+
+	timeout := time.Hour
+	var flag atomic.Bool
+
+	server := onidle.NewServer(ctx, func() {
+		flag.Store(true)
+	}, onidle.WithTimeout(timeout))
+
+	clockMock.Add(timeout - 1)
+	conn1, err := server.Request(ctx, &networkservice.NetworkServiceRequest{
+		Connection: &networkservice.Connection{
+			Id: "1",
+		},
+	})
+	require.NoError(t, err)
+	conn2, err := server.Request(ctx, &networkservice.NetworkServiceRequest{
+		Connection: &networkservice.Connection{
+			Id: "2",
+		},
+	})
+	require.NoError(t, err)
+
+	_, err = server.Close(ctx, conn1)
+	require.NoError(t, err)
+	clockMock.Add(timeout)
+	require.Never(t, flag.Load, testWait, testTick)
+	_, err = server.Close(ctx, conn2)
+	require.NoError(t, err)
+	clockMock.Add(timeout)
+	require.Eventually(t, flag.Load, testWait, testTick)
+}
+
+func TestIdleNotifier_FailedRequest(t *testing.T) {
+	t.Cleanup(func() { goleak.VerifyNone(t) })
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	clockMock := clockmock.New()
+	ctx = clock.WithClock(ctx, clockMock)
+
+	timeout := time.Hour
+	var flag atomic.Bool
+
+	server := next.NewNetworkServiceServer(
+		onidle.NewServer(ctx, func() {
+			flag.Store(true)
+		}, onidle.WithTimeout(timeout)),
+		injecterror.NewServer(),
+	)
+
+	clockMock.Add(timeout - 1)
+	require.Never(t, flag.Load, testWait, testTick)
+
+	_, err := server.Request(ctx, &networkservice.NetworkServiceRequest{})
+	require.Error(t, err)
+	clockMock.Add(timeout - 1)
+	require.Never(t, flag.Load, testWait, testTick)
+
+	clockMock.Add(1)
+	require.Eventually(t, flag.Load, testWait, testTick)
+}
+
+func TestIdleNotifier_ContextCancel(t *testing.T) {
+	t.Cleanup(func() { goleak.VerifyNone(t) })
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	clockMock := clockmock.New()
+	ctx = clock.WithClock(ctx, clockMock)
+
+	timeout := time.Hour
+	var flag atomic.Bool
+
+	server := onidle.NewServer(ctx, func() {
+		flag.Store(true)
+	}, onidle.WithTimeout(timeout))
+
+	cancel()
+	clockMock.Add(timeout)
+	require.Never(t, flag.Load, testWait, testTick)
+
+	_, err := server.Request(ctx, &networkservice.NetworkServiceRequest{})
+	require.NoError(t, err)
+}
+
+func TestIdleNotifier_RequestAfterExpire(t *testing.T) {
+	t.Cleanup(func() { goleak.VerifyNone(t) })
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	clockMock := clockmock.New()
+	ctx = clock.WithClock(ctx, clockMock)
+
+	timeout := time.Hour
+	var flag atomic.Bool
+
+	server := onidle.NewServer(ctx, func() {
+		flag.Store(true)
+	}, onidle.WithTimeout(timeout))
+
+	clockMock.Add(timeout)
+	require.Eventually(t, flag.Load, time.Second, testTick)
+	_, err := server.Request(ctx, &networkservice.NetworkServiceRequest{})
+	require.Error(t, err)
+}
