576eb22c6495ee7e27591cf241a22e7bb9d25fbc
Vladimir Popov <vladimir.popov@xored.com>
2021-07-29 18:15:06 +0700

    Add switch-case server, client chain elements
    Signed-off-by: Vladimir Popov <vladimir.popov@xored.com>

---
+++ pkg/networkservice/common/swich/default.go
@@ -0,0 +1,31 @@
+// Copyright (c) 2021 Doc.ai and/or its affiliates.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package swich
+
+import (
+	"context"
+
+	"github.com/networkservicemesh/api/pkg/api/networkservice"
+)
+
+// Condition is a type for swich.*Case condition
+type Condition = func(context.Context, *networkservice.Connection) bool
+
+// Default is a "default" case condition for swich
+func Default(context.Context, *networkservice.Connection) bool {
+	return true
+}
---
+++ pkg/networkservice/common/swich/server.go
@@ -0,0 +1,61 @@
+// Copyright (c) 2021 Doc.ai and/or its affiliates.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package swich
+
+import (
+	"context"
+
+	"github.com/pkg/errors"
+	"google.golang.org/protobuf/types/known/emptypb"
+
+	"github.com/networkservicemesh/api/pkg/api/networkservice"
+)
+
+// ServerCase is a case type for the switch-case server chain element
+type ServerCase struct {
+	Condition Condition
+	Server    networkservice.NetworkServiceServer
+}
+
+type switchServer struct {
+	cases []*ServerCase
+}
+
+// NewServer returns a new switch-case server chain element
+func NewServer(cases ...*ServerCase) networkservice.NetworkServiceServer {
+	return &switchServer{
+		cases: cases,
+	}
+}
+
+func (s *switchServer) Request(ctx context.Context, request *networkservice.NetworkServiceRequest) (*networkservice.Connection, error) {
+	for _, c := range s.cases {
+		if c.Condition(ctx, request.GetConnection()) {
+			return c.Server.Request(ctx, request)
+		}
+	}
+	return nil, errors.WithStack(errors.New("all cases failed"))
+}
+
+func (s *switchServer) Close(ctx context.Context, conn *networkservice.Connection) (*emptypb.Empty, error) {
+	for _, c := range s.cases {
+		if c.Condition(ctx, conn) {
+			return c.Server.Close(ctx, conn)
+		}
+	}
+	return nil, errors.WithStack(errors.New("all cases failed"))
+}
