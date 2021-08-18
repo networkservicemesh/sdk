51c735cb495eee138d0f33ecd14d7302a1987774
Vladimir Popov <vladimir.popov@xored.com>
2021-08-18 21:14:40 +0700

    [sdk-vpp#314] Add switch-case chain elements (#1046)
    * Add switch-case server, client chain elements
    
    Signed-off-by: Vladimir Popov <vladimir.popov@xored.com>
    
    * Rename swich package to switchcase
    
    Signed-off-by: Vladimir Popov <vladimir.popov@xored.com>
    
    * Rework switchcase to call next
    
    Signed-off-by: Vladimir Popov <vladimir.popov@xored.com>

---
+++ pkg/networkservice/common/switchcase/client.go
@@ -0,0 +1,63 @@
+// Copyright (c) 2021 Doc.ai and/or its affiliates.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package switchcase
+
+import (
+	"context"
+
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/emptypb"
+
+	"github.com/networkservicemesh/api/pkg/api/networkservice"
+
+	"github.com/networkservicemesh/sdk/pkg/networkservice/core/next"
+)
+
+// ClientCase is a case type for the switch-case client chain element
+type ClientCase struct {
+	Condition Condition
+	Client    networkservice.NetworkServiceClient
+}
+
+type switchClient struct {
+	cases []*ClientCase
+}
+
+// NewClient returns a new switch-case client chain element
+func NewClient(cases ...*ClientCase) networkservice.NetworkServiceClient {
+	return &switchClient{
+		cases: cases,
+	}
+}
+
+func (s *switchClient) Request(ctx context.Context, request *networkservice.NetworkServiceRequest, opts ...grpc.CallOption) (*networkservice.Connection, error) {
+	for _, c := range s.cases {
+		if c.Condition(ctx, request.GetConnection()) {
+			return c.Client.Request(ctx, request, opts...)
+		}
+	}
+	return next.Client(ctx).Request(ctx, request, opts...)
+}
+
+func (s *switchClient) Close(ctx context.Context, conn *networkservice.Connection, opts ...grpc.CallOption) (*emptypb.Empty, error) {
+	for _, c := range s.cases {
+		if c.Condition(ctx, conn) {
+			return c.Client.Close(ctx, conn, opts...)
+		}
+	}
+	return next.Client(ctx).Close(ctx, conn, opts...)
+}
---
+++ pkg/networkservice/common/switchcase/common_test.go
@@ -0,0 +1,75 @@
+// Copyright (c) 2021 Doc.ai and/or its affiliates.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package switchcase_test
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/networkservicemesh/api/pkg/api/networkservice"
+
+	"github.com/networkservicemesh/sdk/pkg/networkservice/common/switchcase"
+)
+
+type sample struct {
+	name       string
+	conditions []switchcase.Condition
+	result     int
+}
+
+func testSamples() (samples []*sample) {
+	for i := 0; i < 8; i++ {
+		s := &sample{
+			conditions: make([]switchcase.Condition, 3),
+			result:     -1,
+		}
+
+		bits := i
+		for k := range s.conditions {
+			bit := bits % 2
+			s.conditions[k] = condition(bit)
+			if bit == 1 {
+				s.name = fmt.Sprint(s.name, "true ")
+				if s.result == -1 {
+					s.result = k
+				}
+			} else {
+				s.name = fmt.Sprint(s.name, "false ")
+			}
+			bits /= 2
+		}
+		s.name = fmt.Sprint(s.name, s.result)
+
+		samples = append(samples, s)
+	}
+	return samples
+}
+
+func condition(n int) switchcase.Condition {
+	return func(ctx context.Context, _ *networkservice.Connection) bool {
+		if value := ctx.Value("key"); value != nil {
+			num, ok := value.(int)
+			return ok && num == n
+		}
+		return false
+	}
+}
+
+func withN(ctx context.Context, n int) context.Context {
+	// nolint:golint,staticcheck
+	return context.WithValue(ctx, "key", n)
+}
