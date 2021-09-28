cf8d38b6312c52582cc8c9e6e78125453f095779
Periyasamy Palanisamy <periyasamy.palanisamy@est.tech>
2021-09-28 13:39:50 +0200

    use options closure to set vlan id range
    Signed-off-by: Periyasamy Palanisamy <periyasamy.palanisamy@est.tech>

--- pkg/networkservice/common/mechanisms/kernel/option.go
+++ pkg/networkservice/common/mechanisms/kernel/option.go
@@ -16,8 +16,11 @@
 
 package kernel
 
+import "github.com/RoaringBitmap/roaring"
+
 type options struct {
 	interfaceName string
+	vlans         *roaring.Bitmap
 }
 
 // Option is an option pattern for kernelMechanismClient/Server
@@ -27,5 +30,13 @@ type Option func(o *options)
 func WithInterfaceName(interfaceName string) Option {
 	return func(o *options) {
 		o.interfaceName = limitName(interfaceName)
+	}
+}
+
+// WithVLAN sets assignable vlan id range
+func WithVLAN() Option {
+	return func(o *options) {
+		o.vlans = roaring.New()
+		o.vlans.AddRange(1, 4095)
 	}
 }
