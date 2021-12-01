ef84a3107d05d98f60a830ef6999830b563856a8
Nikita Skrynnik <nikita.skrynnik@xored.com>
2021-12-01 19:22:57 +0700

    searching for long part of test (10)
    Signed-off-by: Nikita Skrynnik <nikita.skrynnik@xored.com>

--- pkg/networkservice/core/trace/common.go
+++ pkg/networkservice/core/trace/common.go
@@ -24,6 +24,7 @@ import (
 	"fmt"
 	"reflect"
 	"strconv"
+	"strings"
 
 	"github.com/pkg/errors"
 	"google.golang.org/protobuf/proto"
@@ -33,40 +34,40 @@ import (
 )
 
 func logRequest(ctx context.Context, request proto.Message, prefixes ...string) {
-	// msg := strings.Join(prefixes, "-")
-	// diffMsg := strings.Join(append(prefixes, "diff"), "-")
+	msg := strings.Join(prefixes, "-")
+	diffMsg := strings.Join(append(prefixes, "diff"), "-")
 
-	// connInfo, ok := trace(ctx)
-	// if ok && !proto.Equal(connInfo.Request, request) {
-	// 	if connInfo.Request != nil && connInfo.Request.ProtoReflect().Descriptor().FullName() == request.ProtoReflect().Descriptor().FullName() {
-	// 		requestDiff, hadChanges := Diff(connInfo.Request.ProtoReflect(), request.ProtoReflect())
-	// 		if hadChanges {
-	// 			logObjectTrace(ctx, diffMsg, requestDiff)
-	// 		}
-	// 	} else {
-	// 		logObjectTrace(ctx, msg, request)
-	// 	}
-	// 	connInfo.Request = proto.Clone(request)
-	// }
+	connInfo, ok := trace(ctx)
+	if ok /* && !proto.Equal(connInfo.Request, request) */ {
+		if connInfo.Request != nil && connInfo.Request.ProtoReflect().Descriptor().FullName() == request.ProtoReflect().Descriptor().FullName() {
+			requestDiff, hadChanges := Diff(connInfo.Request.ProtoReflect(), request.ProtoReflect())
+			if hadChanges {
+				logObjectTrace(ctx, diffMsg, requestDiff)
+			}
+		} else {
+			logObjectTrace(ctx, msg, request)
+		}
+		connInfo.Request = proto.Clone(request)
+	}
 }
 
 func logResponse(ctx context.Context, response proto.Message, prefixes ...string) {
-	// msg := strings.Join(append(prefixes, "response"), "-")
-	// diffMsg := strings.Join(append(prefixes, "response", "diff"), "-")
+	msg := strings.Join(append(prefixes, "response"), "-")
+	diffMsg := strings.Join(append(prefixes, "response", "diff"), "-")
 
-	// connInfo, ok := trace(ctx)
-	// if ok && !proto.Equal(connInfo.Response, response) {
-	// 	if connInfo.Response != nil {
-	// 		responseDiff, changed := Diff(connInfo.Response.ProtoReflect(), response.ProtoReflect())
-	// 		if changed {
-	// 			logObjectTrace(ctx, diffMsg, responseDiff)
-	// 		}
-	// 	} else {
-	// 		logObjectTrace(ctx, msg, response)
-	// 	}
-	// 	connInfo.Response = proto.Clone(response)
-	// 	return
-	// }
+	connInfo, ok := trace(ctx)
+	if ok /* && !proto.Equal(connInfo.Response, response) */ {
+		if connInfo.Response != nil {
+			responseDiff, changed := Diff(connInfo.Response.ProtoReflect(), response.ProtoReflect())
+			if changed {
+				logObjectTrace(ctx, diffMsg, responseDiff)
+			}
+		} else {
+			logObjectTrace(ctx, msg, response)
+		}
+		connInfo.Response = proto.Clone(response)
+		return
+	}
 }
 
 // Diff - calculate a protobuf message diff
