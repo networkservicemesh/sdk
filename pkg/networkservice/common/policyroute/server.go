419fa7053a1a24ebe3b0812bb2ee1849e3b60a4b
Artem Glazychev <artem.glazychev@xored.com>
2022-01-24 14:38:03 +0700

    Add policyroute chain element (#1216)
    Signed-off-by: Artem Glazychev <artem.glazychev@xored.com>

---
+++ pkg/networkservice/common/policyroute/server.go
@@ -0,0 +1,79 @@
+// Copyright (c) 2022 Doc.ai and/or its affiliates.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package policyroute
+
+import (
+	"context"
+
+	"github.com/golang/protobuf/ptypes/empty"
+	"github.com/networkservicemesh/api/pkg/api/networkservice"
+
+	"github.com/networkservicemesh/sdk/pkg/networkservice/core/next"
+)
+
+// PolicyRoutesFunc - method for the new policyRoutes getting
+type PolicyRoutesFunc func() []*networkservice.PolicyRoute
+
+type policyrouteServer struct {
+	getPolicies PolicyRoutesFunc
+}
+
+// NewServer creates a NetworkServiceServer that will put the routing policies to connection context
+func NewServer(policyRouteGetter PolicyRoutesFunc) networkservice.NetworkServiceServer {
+	return &policyrouteServer{
+		getPolicies: policyRouteGetter,
+	}
+}
+
+func (p *policyrouteServer) Request(ctx context.Context, request *networkservice.NetworkServiceRequest) (*networkservice.Connection, error) {
+	conn := request.GetConnection()
+	if conn.GetContext() == nil {
+		conn.Context = &networkservice.ConnectionContext{}
+	}
+	if conn.GetContext().GetIpContext() == nil {
+		conn.GetContext().IpContext = &networkservice.IPContext{}
+	}
+	ipContext := conn.GetContext().GetIpContext()
+
+	// Update policies
+	// Remove old IP addresses
+	policies := p.getPolicies()
+	for _, p := range ipContext.Policies {
+		if p.From != "" {
+			for s := range ipContext.SrcIpAddrs {
+				if ipContext.SrcIpAddrs[s] == p.From {
+					ipContext.SrcIpAddrs = append(ipContext.SrcIpAddrs[:s], ipContext.SrcIpAddrs[s+1:]...)
+					break
+				}
+			}
+		}
+	}
+	// Use new policies
+	ipContext.Policies = policies
+
+	// Add new IP addresses
+	for _, p := range policies {
+		if p.From != "" {
+			ipContext.SrcIpAddrs = append(ipContext.SrcIpAddrs, p.From)
+		}
+	}
+	return next.Server(ctx).Request(ctx, request)
+}
+
+func (p *policyrouteServer) Close(ctx context.Context, conn *networkservice.Connection) (*empty.Empty, error) {
+	return next.Server(ctx).Close(ctx, conn)
+}
