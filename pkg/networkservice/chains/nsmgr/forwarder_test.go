1c05aac51b2754ab5b1e61e987fc35ef23cd4df4
Nikita Skrynnik <nikita.skrynnik@xored.com>
2022-02-25 22:19:43 +0700

    enable tracing
    Signed-off-by: Nikita Skrynnik <nikita.skrynnik@xored.com>

--- pkg/networkservice/chains/nsmgr/forwarder_test.go
+++ pkg/networkservice/chains/nsmgr/forwarder_test.go
@@ -27,6 +27,7 @@ import (
 	"github.com/stretchr/testify/require"
 	"go.uber.org/goleak"
 
+	"github.com/networkservicemesh/sdk/pkg/tools/log"
 	"github.com/networkservicemesh/sdk/pkg/tools/sandbox"
 )
 
@@ -55,8 +56,8 @@ func testForwarderShouldBeSelectedCorrectlyOnNSMgrRestart(t *testing.T, nodeNum,
 	t.Cleanup(func() { goleak.VerifyNone(t) })
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*400)
-	// logrus.SetLevel(logrus.TraceLevel)
-	// log.EnableTracing(true)
+	logrus.SetLevel(logrus.TraceLevel)
+	log.EnableTracing(true)
 	defer cancel()
 
 	domain := sandbox.NewBuilder(ctx, t).
