4c1ffbf8857f7c75c7dc0031288d5e533cc0ab08
Nikita Skrynnik <nikita.skrynnik@xored.com>
2022-03-15 18:16:17 +0700

    fix linter issues
    Signed-off-by: Nikita Skrynnik <nikita.skrynnik@xored.com>

--- pkg/tools/matchutils/utils.go
+++ pkg/tools/matchutils/utils.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2020-2021 Doc.ai and/or its affiliates.
+// Copyright (c) 2020-2022 Doc.ai and/or its affiliates.
 //
 // SPDX-License-Identifier: Apache-2.0
 //
@@ -75,7 +75,6 @@ func CountIntersections(a, b, values map[string]string) int {
 				matchCount++
 			}
 		}
-
 	}
 
 	return matchCount
