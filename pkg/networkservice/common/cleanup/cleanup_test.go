b88289b9022eb478dd1b0470dc2066417b508534
Artem Glazychev <artem.glazychev@xored.com>
2022-03-16 17:50:41 +0700

    Add cleanup chain element (#1246)
    * Add cleanup chain element
    
    Signed-off-by: Artem Glazychev <artem.glazychev@xored.com>
    
    * Rename option
    
    Signed-off-by: Artem Glazychev <artem.glazychev@xored.com>

---
+++ pkg/networkservice/common/cleanup/cleanup_test.go
@@ -0,0 +1,120 @@
+// Copyright (c) 2022 Cisco and/or its affiliates.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cleanup_test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"go.uber.org/goleak"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/networkservicemesh/api/pkg/api/networkservice"
+
+	"github.com/networkservicemesh/sdk/pkg/networkservice/common/begin"
+	"github.com/networkservicemesh/sdk/pkg/networkservice/common/cleanup"
+	"github.com/networkservicemesh/sdk/pkg/networkservice/core/chain"
+	"github.com/networkservicemesh/sdk/pkg/networkservice/utils/count"
+	"github.com/networkservicemesh/sdk/pkg/networkservice/utils/metadata"
+)
+
+func TestCleanUp_CtxDone(t *testing.T) {
+	t.Cleanup(func() { goleak.VerifyNone(t) })
+	chainCtx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	counter := new(count.Client)
+
+	client := chain.NewNetworkServiceClient(
+		begin.NewClient(),
+		metadata.NewClient(),
+		cleanup.NewClient(chainCtx),
+		counter,
+	)
+	req := &networkservice.NetworkServiceRequest{
+		Connection: &networkservice.Connection{Id: "nsc-1"},
+	}
+	_, err := client.Request(context.Background(), req)
+	require.NoError(t, err)
+	require.Equal(t, 1, counter.Requests())
+	require.Equal(t, 0, counter.Closes())
+	cancel()
+
+	require.Eventually(t, func() bool {
+		return counter.Closes() == 1
+	}, time.Millisecond*100, time.Millisecond*10)
+}
+
+func TestCleanUp_Close(t *testing.T) {
+	t.Cleanup(func() { goleak.VerifyNone(t) })
+	chainCtx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	counter := new(count.Client)
+
+	client := chain.NewNetworkServiceClient(
+		begin.NewClient(),
+		metadata.NewClient(),
+		cleanup.NewClient(chainCtx),
+		counter,
+	)
+	req := &networkservice.NetworkServiceRequest{
+		Connection: &networkservice.Connection{Id: "nsc-1"},
+	}
+	conn, err := client.Request(context.Background(), req)
+	require.NoError(t, err)
+
+	_, _ = client.Close(context.Background(), conn)
+	require.Equal(t, 1, counter.Closes())
+	cancel()
+	require.Never(t, func() bool {
+		return counter.Closes() > 1
+	}, time.Millisecond*100, time.Millisecond*10)
+}
+
+func TestCleanUp_Chan(t *testing.T) {
+	t.Cleanup(func() { goleak.VerifyNone(t) })
+	chainCtx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	counter := new(count.Client)
+
+	doneCh := make(chan struct{})
+	client := chain.NewNetworkServiceClient(
+		begin.NewClient(),
+		metadata.NewClient(),
+		cleanup.NewClient(chainCtx, cleanup.WithDoneChan(doneCh)),
+		counter,
+	)
+
+	requestsNumber := 500
+	for i := 0; i < requestsNumber; i++ {
+		req := &networkservice.NetworkServiceRequest{
+			Connection: &networkservice.Connection{Id: fmt.Sprintf("nsc-%v", i)},
+		}
+		_, err := client.Request(context.Background(), req)
+		require.NoError(t, err)
+	}
+
+	cancel()
+	<-doneCh
+
+	require.Equal(t, counter.Closes(), requestsNumber)
+}
---
+++ pkg/networkservice/common/cleanup/metadata.go
@@ -0,0 +1,58 @@
+// Copyright (c) 2022 Cisco and/or its affiliates.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cleanup
+
+import (
+	"context"
+
+	"github.com/networkservicemesh/sdk/pkg/networkservice/utils/metadata"
+)
+
+type keyCancel struct{}
+type keyCC struct{}
+
+// storeCancel sets the context.CancelFunc stored in per Connection.Id metadata.
+func storeCancel(ctx context.Context, cancel context.CancelFunc) {
+	metadata.Map(ctx, true).Store(keyCancel{}, cancel)
+}
+
+// loadAndDeleteCancel deletes the context.CancelFunc stored in per Connection.Id metadata,
+// returning the previous value if any. The loaded result reports whether the key was present.
+func loadAndDeleteCancel(ctx context.Context) (value context.CancelFunc, ok bool) {
+	rawValue, ok := metadata.Map(ctx, true).LoadAndDelete(keyCancel{})
+	if !ok {
+		return
+	}
+	value, ok = rawValue.(context.CancelFunc)
+	return value, ok
+}
+
+// storeCC sets the flag to delete clientconn in per Connection.Id metadata.
+func storeCC(ctx context.Context) {
+	metadata.Map(ctx, true).Store(keyCC{}, struct{}{})
+}
+
+// loadAndDeleteCC deletes the flag stored in per Connection.Id metadata,
+// returning the previous value if any. The loaded result reports whether the key was present.
+func loadAndDeleteCC(ctx context.Context) (value struct{}, ok bool) {
+	rawValue, ok := metadata.Map(ctx, true).LoadAndDelete(keyCC{})
+	if !ok {
+		return
+	}
+	value, ok = rawValue.(struct{})
+	return value, ok
+}
