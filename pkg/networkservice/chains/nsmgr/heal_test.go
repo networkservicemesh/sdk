53392d1ba42c39bea6267baef24402b2a1fc5849
Nikita Skrynnik <nikita.skrynnik@xored.com>
2022-03-29 16:13:42 +0700

    add more test launches
    Signed-off-by: Nikita Skrynnik <nikita.skrynnik@xored.com>

--- pkg/networkservice/chains/nsmgr/heal_test.go
+++ pkg/networkservice/chains/nsmgr/heal_test.go
@@ -30,7 +30,6 @@ import (
 
 	"github.com/networkservicemesh/sdk/pkg/networkservice/utils/count"
 	"github.com/networkservicemesh/sdk/pkg/registry/chains/client"
-	"github.com/networkservicemesh/sdk/pkg/tools/log"
 	"github.com/networkservicemesh/sdk/pkg/tools/sandbox"
 )
 
@@ -474,9 +473,6 @@ func testForwarderShouldBeSelectedCorrectlyOnNSMgrRestart(t *testing.T, nodeNum,
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*20)
 	defer cancel()
-
-	log.EnableTracing(true)
-	logrus.SetLevel(logrus.TraceLevel)
 
 	domain := sandbox.NewBuilder(ctx, t).
 		SetNodesCount(nodeNum + 1).
