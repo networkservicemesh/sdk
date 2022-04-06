2eb8cee1dbe848b73cc778bf22190569a83d6a7c
Albert Safin <albert.safin@xored.com>
2022-04-06 21:25:10 +0000

    Run golangci-lint on darwin and windows
    * gofmt *_nolinux.go
    * Remove unused function GetInode()
    
    Signed-off-by: Albert Safin <albert.safin@xored.com>

--- pkg/tools/fs/utils.go
+++ pkg/tools/fs/utils.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2020-2021 Doc.ai and/or its affiliates.
+// Copyright (c) 2020-2022 Doc.ai and/or its affiliates.
 //
 // SPDX-License-Identifier: Apache-2.0
 //
@@ -14,6 +14,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package fs provides common filesystem functions and utilities
 package fs
 
 import (
