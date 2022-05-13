449abeef26a42e79069999be49c3957998bba7be
Denis Tingaikin <denis.tingajkin@xored.com>
2022-05-13 03:01:21 +0300

    fix ci fail
    Signed-off-by: Denis Tingaikin <denis.tingajkin@xored.com>

--- pkg/networkservice/chains/nsmgr/single_test.go
+++ pkg/networkservice/chains/nsmgr/single_test.go
@@ -80,7 +80,7 @@ func Test_DNSUsecase(t *testing.T) {
 	err = ioutil.WriteFile(resolveConfigPath, []byte("nameserver 8.8.4.4\nsearch example.com\n"), os.ModePerm)
 	require.NoError(t, err)
 
-	const expectedCorefile = ". {\n\tforward . 8.8.4.4\n\tlog\n\treload\n}\nmy.domain1 {\n\tfanout . 8.8.4.4 8.8.8.8\n\tlog\n}"
+	const expectedCorefile = ". {\n\tfanout . 8.8.4.4\n\tlog\n\treload\n\tcache\n}\nmy.domain1 {\n\tfanout . 8.8.4.4 8.8.8.8\n\tlog\n\tcache\n}"
 
 	nsc := domain.Nodes[0].NewClient(ctx, sandbox.GenerateTestToken, client.WithAdditionalFunctionality(dnscontext.NewClient(
 		dnscontext.WithChainContext(ctx),
