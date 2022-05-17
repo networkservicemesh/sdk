dd74452e68ad1c7bbc632fb3e32993caa841ccb2
Nikita Skrynnik <nikita.skrynnik@xored.com>
2022-05-17 15:30:47 +0700

    fix linter
    Signed-off-by: Nikita Skrynnik <nikita.skrynnik@xored.com>

--- pkg/registry/common/clientinfo/client_test.go
+++ pkg/registry/common/clientinfo/client_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2020-2022 Doc.ai and/or its affiliates.
+// Copyright (c) 2022 Doc.ai and/or its affiliates.
 //
 // SPDX-License-Identifier: Apache-2.0
 //
@@ -25,6 +25,7 @@ import (
 	"go.uber.org/goleak"
 
 	"github.com/networkservicemesh/api/pkg/api/registry"
+
 	"github.com/networkservicemesh/sdk/pkg/registry/common/clientinfo"
 )
 
