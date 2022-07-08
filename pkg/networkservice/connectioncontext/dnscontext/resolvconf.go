912b6259ca001379a6b460a416cad5695dfc4467
Nikita Skrynnik <nikita.skrynnik@xored.com>
2022-07-08 13:23:09 +1100

    fix linter
    Signed-off-by: Nikita Skrynnik <nikita.skrynnik@xored.com>

--- pkg/networkservice/connectioncontext/dnscontext/resolvconf.go
+++ pkg/networkservice/connectioncontext/dnscontext/resolvconf.go
@@ -16,7 +16,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package resolvconf provides API for editing / reading resolv.conf
 package dnscontext
 
 import (
