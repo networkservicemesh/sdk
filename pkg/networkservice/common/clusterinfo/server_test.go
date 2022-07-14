5eb7da1bc22fadba25f14f77fc66d2c900ecaa1c
Denis Tingaikin <49399980+denis-tingaikin@users.noreply.github.com>
2022-07-14 18:23:44 +0300

    add cluster-info chain elements (#1326)
    Signed-off-by: Denis Tingaikin <denis.tingajkin@xored.com>
    Signed-off-by: anastasia.malysheva <anastasia.malysheva@xored.com>

---
+++ pkg/networkservice/common/clusterinfo/server_test.go
@@ -0,0 +1,46 @@
+// Copyright (c) 2022 Cisco and/or its affiliates.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package clusterinfo_test
+
+import (
+	"context"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/networkservicemesh/api/pkg/api/networkservice"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/goleak"
+
+	"github.com/networkservicemesh/sdk/pkg/networkservice/common/clusterinfo"
+)
+
+func TestReadClusterName(t *testing.T) {
+	t.Cleanup(func() { goleak.VerifyNone(t) })
+
+	var path = filepath.Join(t.TempDir(), "clusterinfo.yaml")
+	require.NoError(t, ioutil.WriteFile(path, []byte("CLUSTER_NAME: my-cluster1"), os.ModePerm))
+
+	var s = clusterinfo.NewServer(clusterinfo.WithConfigPath(path))
+
+	var resp, err = s.Request(context.Background(), &networkservice.NetworkServiceRequest{Connection: &networkservice.Connection{}})
+	require.NoError(t, err)
+
+	require.Len(t, resp.Labels, 1)
+	require.Equal(t, "my-cluster1", resp.GetLabels()["CLUSTER_NAME"])
+}
