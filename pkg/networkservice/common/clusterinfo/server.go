e129caab480927deb5e00e26ad86bc9079735772
Denis Tingaikin <denis.tingajkin@xored.com>
2022-07-14 04:14:06 +0300

    add cluster-info chain elements
    Signed-off-by: Denis Tingaikin <denis.tingajkin@xored.com>

---
+++ pkg/networkservice/common/clusterinfo/server.go
@@ -0,0 +1,66 @@
+// Copyright (c) 2022 Cisco and/or its affiliates.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package clusterinfo provides a chain element that appends clusterinfo labels into the request.
+package clusterinfo
+
+import (
+	"context"
+	"io/ioutil"
+
+	"github.com/golang/protobuf/ptypes/empty"
+	"github.com/networkservicemesh/api/pkg/api/networkservice"
+	"gopkg.in/yaml.v2"
+
+	"github.com/networkservicemesh/sdk/pkg/networkservice/core/next"
+)
+
+type clusterinfoServer struct {
+	configPath string
+}
+
+// NewServer - returns a new clusterinfo NetworkServiceServer that adds clusterinfo labels into request from the cluterinfo configuration.
+func NewServer(opts ...Option) networkservice.NetworkServiceServer {
+	var r = &clusterinfoServer{
+		configPath: "/etc/clusterinfo/config.yaml",
+	}
+	for _, opt := range opts {
+		opt(r)
+	}
+	return r
+}
+
+func (n *clusterinfoServer) Request(ctx context.Context, request *networkservice.NetworkServiceRequest) (*networkservice.Connection, error) {
+	if request.GetConnection().GetLabels() == nil {
+		request.GetConnection().Labels = make(map[string]string)
+	}
+
+	var m = make(map[string]string)
+
+	if b, err := ioutil.ReadFile(n.configPath); err == nil {
+		_ = yaml.Unmarshal(b, &m)
+	}
+
+	for k, v := range m {
+		request.GetConnection().GetLabels()[k] = v
+	}
+
+	return next.Server(ctx).Request(ctx, request)
+}
+
+func (n *clusterinfoServer) Close(ctx context.Context, conn *networkservice.Connection) (*empty.Empty, error) {
+	return next.Server(ctx).Close(ctx, conn)
+}
---
+++ pkg/registry/common/clusterinfo/nse_server_test.go
@@ -0,0 +1,49 @@
+// Copyright (c) 2022 Cisco and/or its affiliates.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package clusterinfo_test
+
+import (
+	"context"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/networkservicemesh/api/pkg/api/registry"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/goleak"
+
+	"github.com/networkservicemesh/sdk/pkg/registry/common/clusterinfo"
+)
+
+func TestReadClusterName(t *testing.T) {
+	t.Cleanup(func() { goleak.VerifyNone(t) })
+
+	var path = filepath.Join(t.TempDir(), "clusterinfo.yaml")
+	require.NoError(t, ioutil.WriteFile(path, []byte("CLUSTER_NAME: my-cluster1"), os.ModePerm))
+
+	var s = clusterinfo.NewNetworkServiceEndpointRegistryServer(clusterinfo.WithConfigPath(path))
+
+	var resp, err = s.Register(context.Background(), &registry.NetworkServiceEndpoint{NetworkServiceLabels: map[string]*registry.NetworkServiceLabels{
+		"ns-1": {},
+	}})
+	require.NoError(t, err)
+
+	require.Len(t, resp.GetNetworkServiceLabels(), 1)
+	require.Len(t, resp.GetNetworkServiceLabels()["ns-1"].GetLabels(), 1)
+	require.Equal(t, "my-cluster1", resp.GetNetworkServiceLabels()["ns-1"].GetLabels()["CLUSTER_NAME"])
+}
