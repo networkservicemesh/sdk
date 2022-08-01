64b4f6ff74915cd263cacf546b4fd442c920ebd9
Nikita Skrynnik <nikita.skrynnik@xored.com>
2022-08-01 14:39:21 +1100

    fix linter
    Signed-off-by: Nikita Skrynnik <nikita.skrynnik@xored.com>

--- pkg/registry/common/authorize/options.go
+++ pkg/registry/common/authorize/options.go
@@ -1,6 +1,4 @@
-// Copyright (c) 2020-2021 Doc.ai and/or its affiliates.
-//
-// Copyright (c) 2020-2021 Cisco Systems, Inc.
+// Copyright (c) 2022 Cisco and/or its affiliates.
 //
 // SPDX-License-Identifier: Apache-2.0
 //
