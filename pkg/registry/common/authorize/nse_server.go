22381f46f05b6be6d9133855c5ae37e215efc955
Nikita Skrynnik <nikita.skrynnik@xored.com>
2022-12-19 20:14:43 +1100

    check empty path in getSpiffeIDFromPath function
    Signed-off-by: Nikita Skrynnik <nikita.skrynnik@xored.com>

--- pkg/registry/common/authorize/nse_server.go
+++ pkg/registry/common/authorize/nse_server.go
@@ -56,11 +56,7 @@ func (s *authorizeNSEServer) Register(ctx context.Context, nse *registry.Network
 	}
 
 	path := grpcmetadata.PathFromContext(ctx)
-
-	spiffeID, err := getSpiffeIDFromPath(path)
-	if err != nil {
-		return nil, err
-	}
+	spiffeID := getSpiffeIDFromPath(ctx, path)
 
 	index := path.Index
 	var leftSide = &grpcmetadata.Path{
@@ -95,11 +91,7 @@ func (s *authorizeNSEServer) Unregister(ctx context.Context, nse *registry.Netwo
 	}
 
 	path := grpcmetadata.PathFromContext(ctx)
-
-	spiffeID, err := getSpiffeIDFromPath(path)
-	if err != nil {
-		return nil, err
-	}
+	spiffeID := getSpiffeIDFromPath(ctx, path)
 
 	index := path.Index
 	var leftSide = &grpcmetadata.Path{
