d99329507b3386308404581dba2412e828a5a00e
Ruslan Bayandinov <wazsone@ya.ru>
2023-03-01 09:11:07 +0000

    fix errors formatting
    Signed-off-by: Ruslan Bayandinov <wazsone@ya.ru>

--- pkg/networkservice/core/trace/common.go
+++ pkg/networkservice/core/trace/common.go
@@ -2,6 +2,8 @@
 //
 // Copyright (c) 2021 Doc.ai and/or its affiliates.
 //
+// Copyright (c) 2023 Cisco Systems, Inc.
+//
 // SPDX-License-Identifier: Apache-2.0
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
