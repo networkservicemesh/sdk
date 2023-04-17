7653b16186adf34b957c1402bd71c7ef5ec51a1e
Danil Uzlov <DanilUzlov@yandex.ru>
2023-04-17 15:29:17 +0700

    add TestNSMGR_KeepForwarderOnNSEDeath_NoHeal test
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>

--- pkg/networkservice/chains/nsmgr/heal_test.go
+++ pkg/networkservice/chains/nsmgr/heal_test.go
@@ -33,6 +33,7 @@ import (
 	"github.com/networkservicemesh/api/pkg/api/networkservice"
 	"github.com/networkservicemesh/api/pkg/api/registry"
 
+	"github.com/networkservicemesh/sdk/pkg/networkservice/chains/client"
 	nsclient "github.com/networkservicemesh/sdk/pkg/networkservice/chains/client"
 	"github.com/networkservicemesh/sdk/pkg/networkservice/chains/nsmgr"
 	"github.com/networkservicemesh/sdk/pkg/networkservice/common/heal"
@@ -818,13 +819,17 @@ func TestNSMGR_KeepForwarderOnErrors(t *testing.T) {
 	for i := 0; i < skipCount; i++ {
 		errorIndices = append(errorIndices, i)
 	}
-	// then allow one successful request, then one error
+	// then allow 1 successful request, then 1 error
 	errorIndices = append(errorIndices, errorIndices[len(errorIndices)-1]+2) //nolint:gocritic
-	// then allow one successful request, then two errors
-	errorIndices = append(errorIndices, errorIndices[len(errorIndices)-1]+2, errorIndices[len(errorIndices)-1]+3)
+	// then allow 1 successful request, then 3 errors
+	errorIndices = append(errorIndices,
+		errorIndices[len(errorIndices)-1]+2,
+		errorIndices[len(errorIndices)-1]+3,
+		errorIndices[len(errorIndices)-1]+4,
+	)
 	inject := injecterror.NewServer(injecterror.WithCloseErrorTimes(), injecterror.WithRequestErrorTimes(errorIndices...))
 	counter := new(count.Server)
-	nse := domain.Nodes[0].NewEndpoint(ctx, nseReg, sandbox.GenerateTestToken, counter, inject)
+	domain.Nodes[0].NewEndpoint(ctx, nseReg, sandbox.GenerateTestToken, counter, inject)
 
 	request := defaultRequest(nsReg.Name)
 
@@ -845,27 +850,83 @@ func TestNSMGR_KeepForwarderOnErrors(t *testing.T) {
 	require.Equal(t, skipCount+3, counter.Requests())
 	require.Equal(t, selectedFwd, conn.GetPath().GetPathSegments()[2].Name)
 
-	// check forwarder doesn't change after 2 consecutive errors
+	// check forwarder doesn't change after 3 consecutive errors
 	request.Connection = conn
 	conn, err = nsc.Request(ctx, request.Clone())
 	require.NoError(t, err)
 	require.Equal(t, skipCount+1, counter.UniqueRequests())
-	require.Equal(t, skipCount+6, counter.Requests())
+	require.Equal(t, skipCount+7, counter.Requests())
 	require.Equal(t, selectedFwd, conn.GetPath().GetPathSegments()[2].Name)
+}
+
+func TestNSMGR_KeepForwarderOnNSEDeath_NoHeal(t *testing.T) {
+	t.Cleanup(func() { goleak.VerifyNone(t) })
+	ctx, cancel := context.WithTimeout(context.Background(), timeout*1000)
+
+	defer cancel()
+	domain := sandbox.NewBuilder(ctx, t).
+		SetNodesCount(1).
+		SetNSMgrProxySupplier(nil).
+		SetRegistryProxySupplier(nil).
+		SetNodeSetup(func(ctx context.Context, node *sandbox.Node, _ int) {
+			node.NewNSMgr(ctx, "nsmgr", nil, sandbox.GenerateTestToken, nsmgr.NewServer)
+		}).
+		Build()
+
+	const fwdCount = 10
+	for i := 0; i < fwdCount; i++ {
+		domain.Nodes[0].NewForwarder(ctx, &registry.NetworkServiceEndpoint{
+			Name:                sandbox.UniqueName("forwarder-" + fmt.Sprint(i)),
+			NetworkServiceNames: []string{"forwarder"},
+		}, sandbox.GenerateTestToken)
+	}
+
+	nsRegistryClient := domain.NewNSRegistryClient(ctx, sandbox.GenerateTestToken)
+
+	nsReg := defaultRegistryService(t.Name())
+	nsReg, err := nsRegistryClient.Register(ctx, nsReg)
+	require.NoError(t, err)
+
+	nseReg := defaultRegistryEndpoint(nsReg.Name)
+
+	counter := new(count.Server)
+	nse := domain.Nodes[0].NewEndpoint(ctx, nseReg, sandbox.GenerateTestToken, counter)
+
+	request := defaultRequest(nsReg.Name)
+
+	nsc := domain.Nodes[0].NewClient(ctx, sandbox.GenerateTestToken, client.WithHealClient(null.NewClient()))
+
+	conn, err := nsc.Request(ctx, request.Clone())
+	require.NoError(t, err)
+	require.Equal(t, 1, counter.UniqueRequests())
+	require.Equal(t, 1, counter.Requests())
+
+	selectedFwd := conn.GetPath().GetPathSegments()[2].Name
 
 	nse.Cancel()
 
-	// Create the second NSE.
+	// fail a refresh on connection timeout
+	refreshCtx, refreshCancel := context.WithTimeout(ctx, time.Second)
+	defer refreshCancel()
+	request.Connection = conn
+	_, err = nsc.Request(refreshCtx, request.Clone())
+	require.Error(t, err)
+
+	// create a new NSE
 	nseReg2 := defaultRegistryEndpoint(nsReg.Name)
 	nseReg2.Name += "-2"
-	regEntry2 := domain.Nodes[0].NewEndpoint(ctx, nseReg2, sandbox.GenerateTestToken, counter)
+	counter2 := new(count.Server)
+	// inject 1 error to make sure that don't go the "first try forwarder in path" route
+	inject2 := injecterror.NewServer(injecterror.WithCloseErrorTimes(), injecterror.WithRequestErrorTimes(0))
+	regEntry2 := domain.Nodes[0].NewEndpoint(ctx, nseReg2, sandbox.GenerateTestToken, counter2, inject2)
 
-	// check forwarder doesn't change after NSE re-selction
+	// check that forwarder doesn't change after NSE re-selction
 	request.Connection = conn
+	request.Connection.NetworkServiceEndpointName = ""
 	conn, err = nsc.Request(ctx, request.Clone())
 	require.NoError(t, err)
-	require.Equal(t, skipCount+2, counter.UniqueRequests())
-	require.Equal(t, skipCount+8, counter.Requests())
+	require.Equal(t, 2, counter2.UniqueRequests())
+	require.Equal(t, 2, counter2.Requests())
 	require.Equal(t, regEntry2.Name, conn.GetPath().GetPathSegments()[3].Name)
 	require.Equal(t, selectedFwd, conn.GetPath().GetPathSegments()[2].Name)
 }
