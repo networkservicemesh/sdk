af68c7c6e7191a7f3335ce4c003469a036785872
Alexander Peretyatko <alexander.peretyatko@xored.com>
2023-08-04 15:23:25 +0700

    Parallel SDK testing (-trigger build)
    Signed-off-by: Alexander Peretyatko <alexander.peretyatko@xored.com>

--- pkg/networkservice/chains/nsmgr/dnsTests/dns_test.go
+++ pkg/networkservice/chains/nsmgr/dnsTests/dns_test.go
@@ -48,7 +48,7 @@ import (
 	"github.com/networkservicemesh/sdk/pkg/tools/sandbox"
 )
 
-// to trigger build 3
+// to trigger build 4
 func Test_DNSUsecase(t *testing.T) {
 	t.Cleanup(func() { goleak.VerifyNone(t) })
 
