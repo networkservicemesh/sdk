a0975880e8b45bc9a603f6159dee977fafeabd69
NikitaSkrynnik <nikita.skrynnik@xored.com>
2024-07-23 17:01:12 +1100

    fix CI issues
    Signed-off-by: NikitaSkrynnik <nikita.skrynnik@xored.com>

--- pkg/networkservice/ipam/point2pointipam/server_test.go
+++ pkg/networkservice/ipam/point2pointipam/server_test.go
@@ -1,6 +1,6 @@
 // Copyright (c) 2020-2022 Doc.ai and/or its affiliates.
 //
-// Copyright (c) 2022 Cisco and/or its affiliates.
+// Copyright (c) 2022-2024 Cisco and/or its affiliates.
 //
 // SPDX-License-Identifier: Apache-2.0
 //
--- pkg/tools/ippool/ippool.go
+++ pkg/tools/ippool/ippool.go
@@ -1,6 +1,6 @@
 // Copyright (c) 2021-2022 Doc.ai and/or its affiliates.
 //
-// Copyright (c) 2022-2023 Cisco and/or its affiliates.
+// Copyright (c) 2022-2024 Cisco and/or its affiliates.
 //
 // SPDX-License-Identifier: Apache-2.0
 //
@@ -334,6 +334,7 @@ func (tree *IPPool) GetPrefixes() []string {
 	return prefixes
 }
 
+// IPLength returns the length of ip addresses allocated by IP Pool
 func (tree *IPPool) IPLength() int {
 	return tree.ipLength
 }
