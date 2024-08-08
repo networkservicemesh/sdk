f8ce70155ed24323a7bbc6480333e2f460294107
Vladislav Byrgazov <vladislav.byrgazov@xored.com>
2024-08-08 14:04:40 +0500

    Add sdk/pkg/tools/pprof
    Signed-off-by: Vladislav Byrgazov <vladislav.byrgazov@xored.com>

---
+++ pkg/tools/pprof/pprof.go
@@ -0,0 +1,54 @@
+// Copyright (c) 2024 Cisco and/or its affiliates.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package pprof provides ability to enable pprof if required
+package pprof
+
+import (
+	"context"
+	"github.com/networkservicemesh/sdk/pkg/tools/log"
+	"net/http"
+	"net/http/pprof"
+	"time"
+)
+
+// Init - configures pprof http handlers
+func Init(ctx context.Context, port string) {
+	go func() {
+		log.FromContext(ctx).Infof("Profiler is enabled. Listening on %s", port)
+		mux := http.NewServeMux()
+		mux.HandleFunc("/debug/pprof/", pprof.Index)
+		mux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+		mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
+		mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+		mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
+		mux.Handle("/debug/pprof/allocs", pprof.Handler("allocs"))
+		mux.Handle("/debug/pprof/block", pprof.Handler("block"))
+		mux.Handle("/debug/pprof/goroutine", pprof.Handler("goroutine"))
+		mux.Handle("/debug/pprof/heap", pprof.Handler("heap"))
+		mux.Handle("/debug/pprof/mutex", pprof.Handler("mutex"))
+		mux.Handle("/debug/pprof/threadcreate", pprof.Handler("threadcreate"))
+		server := &http.Server{
+			Addr:         "localhost:" + port,
+			Handler:      mux,
+			ReadTimeout:  10 * time.Second,
+			WriteTimeout: 10 * time.Second,
+		}
+		if err := server.ListenAndServe(); err != nil {
+			log.FromContext(ctx).Errorf("Failed to start profiler: %s", err.Error())
+		}
+	}()
+}
