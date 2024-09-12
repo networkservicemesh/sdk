6c3706e6daad23d6582b5dbeff9b69d7e24abab1
denis-tingaikin <denis.tingajkin@xored.com>
2024-09-12 15:08:42 +0300

    disable limit for networkservices
    Signed-off-by: denis-tingaikin <denis.tingajkin@xored.com>

--- pkg/networkservice/chains/client/client.go
+++ pkg/networkservice/chains/client/client.go
@@ -28,7 +28,6 @@ import (
 	"github.com/networkservicemesh/sdk/pkg/networkservice/common/clienturl"
 	"github.com/networkservicemesh/sdk/pkg/networkservice/common/connect"
 	"github.com/networkservicemesh/sdk/pkg/networkservice/common/dial"
-	"github.com/networkservicemesh/sdk/pkg/networkservice/common/limit"
 	"github.com/networkservicemesh/sdk/pkg/networkservice/common/null"
 	"github.com/networkservicemesh/sdk/pkg/networkservice/common/refresh"
 	"github.com/networkservicemesh/sdk/pkg/networkservice/common/trimpath"
@@ -64,7 +63,6 @@ func NewClient(ctx context.Context, clientOpts ...Option) networkservice.Network
 					dial.WithDialOptions(opts.dialOptions...),
 					dial.WithDialTimeout(opts.dialTimeout),
 				),
-				limit.NewClient(),
 			},
 			append(
 				opts.additionalFunctionality,
