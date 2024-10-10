7237a6127239cf2796ff6906ae67acb49660bc11
NikitaSkrynnik <nikita.skrynnik@xored.com>
2024-10-10 21:45:55 +0700

    fix linter issues
    Signed-off-by: NikitaSkrynnik <nikita.skrynnik@xored.com>

--- pkg/registry/common/querycache/option.go
+++ pkg/registry/common/querycache/option.go
@@ -1,5 +1,7 @@
 // Copyright (c) 2021 Doc.ai and/or its affiliates.
 //
+// Copyright (c) 2024 Cisco and/or its affiliates.
+//
 // SPDX-License-Identifier: Apache-2.0
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
