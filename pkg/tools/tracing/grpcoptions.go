03a8b86dc357c6f95fce1c0b3ac60e7dae1f3fd7
NikitaSkrynnik <nikita.skrynnik@xored.com>
2024-12-26 14:08:24 +1100

    remove some excluded rules from golang config
    Signed-off-by: NikitaSkrynnik <nikita.skrynnik@xored.com>

--- pkg/tools/tracing/grpcoptions.go
+++ pkg/tools/tracing/grpcoptions.go
@@ -2,6 +2,8 @@
 //
 // Copyright (c) 2021-2022 Doc.ai and/or its affiliates.
 //
+// Copyright (c) 2024 Cisco Systems, Inc.
+//
 // SPDX-License-Identifier: Apache-2.0
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
